0b99051432e5cbd7df29688ce4a4bc8de49f085a
SirMathhman <sirmathhman@gmail.com>
2024-10-11 12:16:04 -0400

    Generate targets

--- test/java/ApplicationTest.java
+++ test/java/ApplicationTest.java
@@ -1,7 +1,56 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ApplicationTest {
+    public static final Path SOURCE = resolve("java");
+    public static final Path TARGET = resolve("mgs");
+
+    private static Path resolve(String extension) {
+        return Paths.get(".", "ApplicationTest." + extension);
+    }
+
+    private static void runMaybeFail() {
+        try {
+            run();
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    private static void run() throws IOException {
+        if (Files.exists(SOURCE)) {
+            Files.createFile(TARGET);
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(TARGET);
+        Files.deleteIfExists(SOURCE);
+    }
+
     @Test
-    void test() {
+    void generatesNoTarget() {
+        runMaybeFail();
+        assertFalse(Files.exists(TARGET));
+    }
+
+    @Test
+    void generatesTarget() {
+        try {
+            Files.createFile(SOURCE);
+        } catch (IOException e) {
+            fail(e);
+        }
+
+        runMaybeFail();
+        assertTrue(Files.exists(TARGET));
     }
 }
