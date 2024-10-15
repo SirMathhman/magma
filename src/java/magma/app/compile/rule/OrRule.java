1bdbe7614b17a90807e0dfaa663cff5ce5a1a654
SirMathhman <sirmathhman@gmail.com>
2024-10-15 11:40:38 -0400

    Revert "Undo"
    This reverts commit ee12f8036bd8cf0329fe723ecaac9e4a7948b331.

--- src/java/magma/app/compile/rule/OrRule.java
+++ src/java/magma/app/compile/rule/OrRule.java
@@ -6,33 +6,24 @@ import magma.app.compile.GenerateException;
 import magma.app.compile.Node;
 import magma.app.compile.ParseException;
 
-import java.util.Collections;
 import java.util.List;
 
 public record OrRule(List<Rule> rules) implements Rule {
-    private Result<Node, ParseException> parse1(String input) {
+    @Override
+    public Result<Node, ParseException> parse(String input) {
         for (Rule rule : rules) {
-            final var parsed = rule.parse(input).first().orElseThrow();
+            final var parsed = rule.parse(input);
             if (parsed.isOk()) return parsed;
         }
         return new Err<>(new ParseException("Invalid input", input));
     }
 
-    private Result<String, GenerateException> generate1(Node node) {
+    @Override
+    public Result<String, GenerateException> generate(Node node) {
         for (Rule rule : rules) {
-            final var generated = rule.generate(node).first().orElseThrow();
+            final var generated = rule.generate(node);
             if (generated.isOk()) return generated;
         }
         return new Err<>(new GenerateException("Invalid input", node));
-    }
-
-    @Override
-    public RuleResult<Node, ParseException> parse(String input) {
-        return new RuleResult<>(Collections.singletonList(parse1(input)));
-    }
-
-    @Override
-    public RuleResult<String, GenerateException> generate(Node node) {
-        return new RuleResult<>(Collections.singletonList(generate1(node)));
     }
 }
