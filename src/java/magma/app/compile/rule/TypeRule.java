ee12f8036bd8cf0329fe723ecaac9e4a7948b331
SirMathhman <sirmathhman@gmail.com>
2024-10-15 11:40:23 -0400

    Undo

--- src/java/magma/app/compile/rule/TypeRule.java
+++ src/java/magma/app/compile/rule/TypeRule.java
@@ -6,15 +6,25 @@ import magma.app.compile.ParseException;
 import magma.api.result.Err;
 import magma.api.result.Result;
 
+import java.util.Collections;
+
 public record TypeRule(String type, Rule rule) implements Rule {
-    @Override
-    public Result<Node, ParseException> parse(String input) {
-        return rule.parse(input).mapValue(node -> node.retype(type));
+    private Result<Node, ParseException> parse1(String input) {
+        return rule.parse(input).first().orElseThrow().mapValue(node -> node.retype(type));
+    }
+
+    private Result<String, GenerateException> generate1(Node node) {
+        if (!node.is(type)) return new Err<>(new GenerateException("Expected a type of '" + type + "'", node));
+        return rule.generate(node).first().orElseThrow();
     }
 
     @Override
-    public Result<String, GenerateException> generate(Node node) {
-        if (!node.is(type)) return new Err<>(new GenerateException("Expected a type of '" + type + "'", node));
-        return rule.generate(node);
+    public RuleResult<Node, ParseException> parse(String input) {
+        return new RuleResult<>(Collections.singletonList(parse1(input)));
+    }
+
+    @Override
+    public RuleResult<String, GenerateException> generate(Node node) {
+        return new RuleResult<>(Collections.singletonList(generate1(node)));
     }
 }
