0c790e824a91545dcc52b4243efcccd6ec9eb13c
SirMathhman <sirmathhman@gmail.com>
2024-10-20 17:39:12 -0400

    Repair error set

--- src/java/magma/app/compile/rule/SuffixRule.java
+++ src/java/magma/app/compile/rule/SuffixRule.java
@@ -1,13 +1,9 @@
 package magma.app.compile.rule;
 
+import magma.api.result.Err;
 import magma.app.compile.GenerateException;
 import magma.app.compile.Node;
 import magma.app.compile.ParseException;
-import magma.api.result.Err;
-import magma.api.result.Ok;
-import magma.api.result.Result;
-
-import java.util.Optional;
 
 public final class SuffixRule implements Rule {
     private final String suffix;
@@ -18,35 +14,16 @@ public final class SuffixRule implements Rule {
         this.childRule = childRule;
     }
 
-    private Optional<Node> parse0(String input) {
-        if (!input.endsWith(suffix)) return Optional.empty();
-        final var slice = input.substring(0, input.length() - suffix.length());
-        return childRule.parse(slice).unwrap().findValue();
-    }
-
-    private Optional<String> generate0(Node node) {
-        return childRule.generate(node).unwrap().findValue().map(value -> value + suffix);
-    }
-
-    private Result<Node, ParseException> parse2(String input) {
-        return parse0(input)
-                .<Result<Node, ParseException>>map(Ok::new)
-                .orElseGet(() -> new Err<Node, ParseException>(new ParseException("Unknown input", input)));
-    }
-
-    private Result<String, GenerateException> generate2(Node node) {
-        return generate0(node)
-                .<Result<String, GenerateException>>map(Ok::new)
-                .orElseGet(() -> new Err<>(new GenerateException("Unknown node", node)));
-    }
-
     @Override
     public RuleResult<Node, ParseException> parse(String input) {
-        return new RuleResult<>(parse2(input));
+        if (!input.endsWith(suffix))
+            return new RuleResult<>(new Err<>(new ParseException("Suffix '" + suffix + "' not present", input)));
+        final var slice = input.substring(0, input.length() - suffix.length());
+        return childRule.parse(slice);
     }
 
     @Override
     public RuleResult<String, GenerateException> generate(Node node) {
-        return new RuleResult<>(generate2(node));
+        return childRule.generate(node).mapValue(value -> value + suffix);
     }
 }
--- src/java/magma/app/compile/rule/TypeRule.java
+++ src/java/magma/app/compile/rule/TypeRule.java
@@ -1,28 +1,20 @@
 package magma.app.compile.rule;
 
+import magma.api.result.Err;
 import magma.app.compile.GenerateException;
 import magma.app.compile.Node;
 import magma.app.compile.ParseException;
-import magma.api.result.Err;
-import magma.api.result.Result;
 
 public record TypeRule(String type, Rule rule) implements Rule {
-    private Result<Node, ParseException> parse2(String input) {
-        return rule.parse(input).unwrap().mapValue(node -> node.retype(type));
-    }
-
-    private Result<String, GenerateException> generate2(Node node) {
-        if (!node.is(type)) return new Err<>(new GenerateException("Expected a type of '" + type + "'", node));
-        return rule.generate(node).unwrap();
-    }
-
     @Override
     public RuleResult<Node, ParseException> parse(String input) {
-        return new RuleResult<>(parse2(input));
+        return rule.parse(input).mapValue(node -> node.retype(type));
     }
 
     @Override
     public RuleResult<String, GenerateException> generate(Node node) {
-        return new RuleResult<>(generate2(node));
+        if (!node.is(type))
+            return new RuleResult<>(new Err<>(new GenerateException("Expected a type of '" + type + "'", node)));
+        return rule.generate(node);
     }
 }
