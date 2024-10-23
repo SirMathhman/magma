d4d0734c3fe2b3cef37b6db3f484f4878f226970
SirMathhman <sirmathhman@gmail.com>
2024-10-23 13:34:44 -0400

    Fix bug where lambda statement did not parse correctly

--- src/java/magma/app/compile/lang/ValueSplitter.java
+++ src/java/magma/app/compile/lang/ValueSplitter.java
@@ -3,7 +3,10 @@ package magma.app.compile.lang;
 import magma.app.compile.rule.Splitter;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ValueSplitter implements Splitter {
     private static void advance(StringBuilder buffer, ArrayList<String> lines) {
@@ -15,8 +18,19 @@ public class ValueSplitter implements Splitter {
         var lines = new ArrayList<String>();
         var buffer = new StringBuilder();
         var depth = 0;
-        for (int i = 0; i < input.length(); i++) {
-            var c = input.charAt(i);
+        final var length = input.length();
+        var queue = IntStream.range(0, length)
+                .mapToObj(input::charAt)
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        while (!queue.isEmpty()) {
+            var c = queue.pop();
+            if (c == '-') {
+                if (!queue.isEmpty() && queue.peek() == '>') {
+                    queue.pop();
+                }
+            }
+
             if (c == ',' && depth == 0) {
                 advance(buffer, lines);
                 buffer = new StringBuilder();
