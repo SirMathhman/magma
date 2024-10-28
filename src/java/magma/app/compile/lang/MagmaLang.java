ed167dd93dfec84c8797044be0ea66565345616e
SirMathhman <sirmathhman@gmail.com>
2024-10-27 22:54:16 -0400

    Function modifiers

--- src/java/magma/app/compile/lang/MagmaLang.java
+++ src/java/magma/app/compile/lang/MagmaLang.java
@@ -9,23 +9,35 @@ import static magma.app.compile.lang.CommonLang.*;
 public class MagmaLang {
     public static final String TRAIT_TYPE = "trait";
     public static final String FUNCTION_TYPE = "function";
+    public static final String STRUCT_TYPE = "struct";
+    public static final List<String> MODIFIERS_LIST = List.of(
+            "inline"
+    );
 
     public static Rule createRootRule() {
         return createChildrenRule(new OrRule(List.of(
                 createImportRule(),
-                createTraitRule(),
-                new TypeRule(FUNCTION_TYPE, new EmptyRule()),
+                createFunctionRule(),
+                createCompoundTypeRule(TRAIT_TYPE, "trait "),
+                createCompoundTypeRule(STRUCT_TYPE, "struct "),
+                new TypeRule("impl", new StripRule(new PrefixRule("impl", new ExtractRule("value")))),
                 createWhitespaceRule()
         )));
     }
 
-    private static TypeRule createTraitRule() {
+    private static TypeRule createFunctionRule() {
+        final var afterKeyword = new EmptyRule();
+        final var withModifiers = new LocatingRule(createModifiersRule(MODIFIERS_LIST), new FirstLocator(" def "), afterKeyword);
+        return new TypeRule(FUNCTION_TYPE, new OptionalNodeListRule(MODIFIERS, withModifiers, new PrefixRule("def ", afterKeyword)));
+    }
+
+    private static TypeRule createCompoundTypeRule(String type, String prefix) {
         final var content = createChildrenRule(new OrRule(List.of(
                 new StripRule(new SuffixRule(createDefinitionRule(), ";")),
                 createWhitespaceRule()
         )));
         final var childRule = new LocatingRule(new StripRule(new ExtractRule("name")), new FirstLocator("{"), new SuffixRule(content, "}"));
-        return new TypeRule(TRAIT_TYPE, new StripRule(new PrefixRule("trait ", childRule)));
+        return new TypeRule(type, new StripRule(new PrefixRule(prefix, childRule)));
     }
 
     private static TypeRule createDefinitionRule() {
