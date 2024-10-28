258304357c86c213a4063a41af2ef68a32bb702f
SirMathhman <sirmathhman@gmail.com>
2024-10-27 22:28:19 -0400

    Empty?

--- src/java/magma/app/compile/lang/MagmaLang.java
+++ src/java/magma/app/compile/lang/MagmaLang.java
@@ -1,33 +1,13 @@
 package magma.app.compile.lang;
 
-import magma.app.compile.rule.*;
-
-import java.util.List;
-
-import static magma.app.compile.lang.CommonLang.*;
+import magma.app.compile.rule.EmptyRule;
+import magma.app.compile.rule.Rule;
 
 public class MagmaLang {
     public static final String TRAIT_TYPE = "trait";
     public static final String FUNCTION_TYPE = "function";
 
     public static Rule createRootRule() {
-        return CommonLang.createRootRule(new OrRule(List.of(
-                createImportRule(),
-                createTraitRule(),
-                new TypeRule(FUNCTION_TYPE, new PrefixRule("class def ", new SuffixRule(new ExtractRule("name"), "() => {}"))),
-                CommonLang.createWhitespaceRule()
-        )));
-    }
-
-    private static TypeRule createTraitRule() {
-        final var name = new ExtractRule("name");
-        final var withTypeParams = new LocatingRule(name, new FirstLocator("<"), new SuffixRule(createTypeParamsRule(createTypeRule()), ">"));
-        final var withoutTypeParams = new SuffixRule(name, " ");
-        final var maybeTypeParams = new OptionalNodeListRule(TYPE_PARAMS, withTypeParams, withoutTypeParams);
-        return new TypeRule(TRAIT_TYPE, new PrefixRule("trait ", new SuffixRule(maybeTypeParams, "{}")));
-    }
-
-    private static TypeRule createTypeRule() {
-        return createSymbolTypeRule();
+        return new EmptyRule();
     }
 }
