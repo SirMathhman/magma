216d718f3bb972139c08ed893f338fe62b33175b
SirMathhman <sirmathhman@gmail.com>
2024-10-29 09:49:57 -0400

    Generate target

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -2,14 +2,48 @@ package magma;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
+    public static final Path TARGET = Paths.get(".", "ApplicationTest.mgs");
+    public static final Path SOURCE = Paths.get(".", "ApplicationTest.java");
+
+    private static void runOrFail() {
+        try {
+            run();
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    private static void run() throws IOException {
+        if (Files.exists(SOURCE)) {
+            Files.createFile(TARGET);
+        }
+    }
+
+    private static void createSource() {
+        try {
+            Files.createFile(SOURCE);
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
+    @Test
+    void generatesTarget() {
+        createSource();
+        runOrFail();
+        assertTrue(Files.exists(TARGET));
+    }
+
     @Test
     void generatesNoTarget() {
-        assertFalse(Files.exists(Paths.get(".", "ApplicationTest.mgs")));
+        assertFalse(Files.exists(TARGET));
     }
 }
