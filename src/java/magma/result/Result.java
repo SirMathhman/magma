4ba11e6453b091c9d907427d40080e50c82ad792
SirMathhman <sirmathhman@gmail.com>
2024-10-29 10:19:41 -0400

    Extract Application

--- src/java/magma/result/Result.java
+++ src/java/magma/result/Result.java
@@ -1,7 +1,12 @@
 package magma.result;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public interface Result<T, E> {
     void consume(Consumer<T> onValid, Consumer<E> onInvalid);
+
+    <R> Result<R, E> mapValue(Function<T, R> mapper);
+
+    <R> R match(Function<T, R> onValid, Function<E, R> onInvalid);
 }
--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,11 +1,5 @@
 package magma;
 
-import magma.option.None;
-import magma.option.Option;
-import magma.option.Some;
-import magma.result.Err;
-import magma.result.Ok;
-import magma.result.Result;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,61 +12,36 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
-    public static final char EXTENSION_SEPARATOR = '.';
-    public static final String MAGMA_EXTENSION = EXTENSION_SEPARATOR + "mgs";
-    public static final Path TARGET = resolveByExtension(MAGMA_EXTENSION);
-    public static final Path SOURCE = resolveByExtension(EXTENSION_SEPARATOR + "java");
+    public static final Path TARGET = resolveByExtension(Application.MAGMA_EXTENSION);
+    public static final Path SOURCE = resolveByExtension(Application.EXTENSION_SEPARATOR + "java");
 
     private static Path resolveByExtension(String extension) {
         return Paths.get(".").resolve("ApplicationTest" + extension);
     }
 
     private static void runOrFail() {
-        run().ifPresent(Assertions::fail);
-    }
-
-    private static Option<IOException> run() {
-        if (!Files.exists(SOURCE)) return new None<>();
-
-        final var fileName = SOURCE.getFileName().toString();
-        final var nameWithoutExtension = new JavaString(fileName)
-                .firstIndexOfChar(EXTENSION_SEPARATOR)
-                .map(index -> fileName.substring(0, index))
-                .orElse(fileName);
-
-        try {
-            Files.createFile(SOURCE.resolveSibling(nameWithoutExtension + MAGMA_EXTENSION));
-            return new None<>();
-        } catch (IOException e) {
-            return new Some<>(e);
-        }
-    }
-
-    private static void writeSource(String input) {
-        try {
-            Files.writeString(SOURCE, input);
-        } catch (IOException e) {
-            fail(e);
-        }
+        new Application(SOURCE).run().ifPresent(Assertions::fail);
     }
 
     private static void runWithInput(String input) {
-        writeSource(input);
+        Application.writeSafe(SOURCE, input).ifPresent(Assertions::fail);
         runOrFail();
     }
 
-    private static Result<String, IOException> readSafe() {
-        try {
-            return new Ok<>(Files.readString(TARGET));
-        } catch (IOException e) {
-            return new Err<>(e);
-        }
+    private static void assertRun(String input, String output) {
+        runWithInput(input);
+        Application.readSafe(TARGET).consume(value -> assertEquals(output, value), Assertions::fail);
+    }
+
+    @Test
+    void importStatement() {
+        final var value = Application.renderImport();
+        assertRun(value, value);
     }
 
     @Test
     void packageStatement() {
-        runWithInput("package magma;");
-        readSafe().consume(value -> assertEquals("", value), Assertions::fail);
+        assertRun("package magma;", "");
     }
 
     @AfterEach
