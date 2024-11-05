58880f87f2c935693ccf6ce30bcbe5908fa81f40
SirMathhman <sirmathhman@gmail.com>
2024-11-05 12:15:46 -0500

    Split correctly

--- src/java/magma/app/compile/rule/NodeListRule.java
+++ src/java/magma/app/compile/rule/NodeListRule.java
@@ -49,11 +49,12 @@ public class NodeListRule implements Rule {
 
     private static State splitAtChar(State state, char c) {
         final var appended = state.append(c);
-        if (c == ';') {
-            return appended.advance();
-        } else {
-            return appended;
-        }
+        if (c == ';' && appended.isLevel()) return appended.advance();
+        if (c == '}' && appended.isShallow()) return appended.exit().advance();
+
+        if (c == '{') return appended.enter();
+        if (c == '}') return appended.exit();
+        return appended;
     }
 
     @Override
--- src/java/magma/app/compile/rule/State.java
+++ src/java/magma/app/compile/rule/State.java
@@ -6,14 +6,16 @@ import java.util.List;
 public class State {
     private final List<String> segments;
     private final StringBuilder buffer;
+    private final int depth;
 
-    public State(List<String> segments, StringBuilder buffer) {
+    public State(List<String> segments, StringBuilder buffer, int depth) {
         this.segments = segments;
         this.buffer = buffer;
+        this.depth = depth;
     }
 
     public State() {
-        this(new ArrayList<>(), new StringBuilder());
+        this(new ArrayList<>(), new StringBuilder(), 0);
     }
 
     State advance() {
@@ -21,14 +23,30 @@ public class State {
 
         final var copy = new ArrayList<>(segments);
         copy.add(buffer.toString());
-        return new State(copy, new StringBuilder());
+        return new State(copy, new StringBuilder(), depth);
     }
 
     State append(char c) {
-        return new State(segments, buffer.append(c));
+        return new State(segments, buffer.append(c), depth);
     }
 
     public List<String> segments() {
         return segments;
+    }
+
+    public boolean isLevel() {
+        return depth == 0;
+    }
+
+    public State enter() {
+        return new State(segments, buffer, depth + 1);
+    }
+
+    public State exit() {
+        return new State(segments, buffer, depth - 1);
+    }
+
+    public boolean isShallow() {
+        return depth == 1;
     }
 }
