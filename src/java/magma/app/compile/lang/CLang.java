6e0a1633b3c93e0727813bbd5dce2669a6ed94c6
SirMathhman <sirmathhman@gmail.com>
2024-11-06 09:42:46 -0500

    Wrap using NodeListRule instead

--- src/java/magma/app/compile/lang/CLang.java
+++ src/java/magma/app/compile/lang/CLang.java
@@ -6,7 +6,7 @@ import static magma.app.compile.lang.CommonLang.createReturnRule;
 
 public class CLang {
     public static Rule createCRootRule() {
-        final var children = new NodeRule(CommonLang.BLOCK_CHILDREN, createReturnRule());
+        final var children = new NodeListRule(CommonLang.BLOCK_CHILDREN, createReturnRule());
         return new TypeRule(CommonLang.FUNCTION_TYPE, new PrefixRule("int main(){\n\t", new SuffixRule(children, "\n}")));
     }
 }
--- src/java/magma/app/compile/lang/MagmaLang.java
+++ src/java/magma/app/compile/lang/MagmaLang.java
@@ -4,11 +4,12 @@ import magma.app.compile.rule.*;
 
 import java.util.List;
 
+import static magma.app.compile.lang.CommonLang.BLOCK_CHILDREN;
+
 public class MagmaLang {
     public static Rule createMagmaRootRule() {
-        return new TypeRule(CommonLang.ROOT_TYPE, new OrRule(List.of(
-                new NodeRule(CommonLang.BLOCK_CHILDREN, CommonLang.createReturnRule()),
-                new EmptyRule()
-        )));
+        return new TypeRule(CommonLang.ROOT_TYPE, new NodeListRule(BLOCK_CHILDREN, new OrRule(List.of(
+                CommonLang.createReturnRule()
+        ))));
     }
 }
