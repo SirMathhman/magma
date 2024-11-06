81d5e6298c4a4943c02d172948534abcb79df261
SirMathhman <sirmathhman@gmail.com>
2024-11-06 09:27:43 -0500

    main

--- src/java/magma/app/compile/lang/CLang.java
+++ src/java/magma/app/compile/lang/CLang.java
@@ -1,10 +1,11 @@
 package magma.app.compile.lang;
 
 import magma.app.compile.rule.EmptyRule;
+import magma.app.compile.rule.PrefixRule;
 import magma.app.compile.rule.Rule;
 
 public class CLang {
     public static Rule createCRootRule() {
-        return new EmptyRule();
+        return new PrefixRule("int main(){\n\treturn 0;\n}", new EmptyRule());
     }
 }
