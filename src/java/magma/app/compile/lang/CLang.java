937d61fa4481bd32a35c702ac3b2bfe78f25dba1
SirMathhman <sirmathhman@gmail.com>
2024-11-07 08:37:56 -0500

    Empty

--- src/java/magma/app/compile/lang/CLang.java
+++ src/java/magma/app/compile/lang/CLang.java
@@ -1,19 +1,10 @@
 package magma.app.compile.lang;
 
-import magma.app.compile.rule.*;
-
-import java.util.List;
-
-import static magma.app.compile.lang.CommonLang.createReturnRule;
+import magma.app.compile.rule.EmptyRule;
+import magma.app.compile.rule.Rule;
 
 public class CLang {
     public static Rule createCRootRule() {
-        final var list = List.of(
-                new TypeRule(CommonLang.DECLARATION_TYPE, new PrefixRule("int x = 420;", new EmptyRule())),
-                createReturnRule()
-        );
-
-        final var children = new NodeRule("value", CommonLang.createBlockRule(list));
-        return new TypeRule(CommonLang.FUNCTION_TYPE, new PrefixRule("int main(){", new SuffixRule(children, "\n}")));
+        return new EmptyRule();
     }
 }
--- src/java/magma/app/compile/lang/MagmaLang.java
+++ src/java/magma/app/compile/lang/MagmaLang.java
@@ -6,9 +6,6 @@ import java.util.List;
 
 public class MagmaLang {
     public static Rule createMagmaRootRule() {
-        return new TypeRule(CommonLang.ROOT_TYPE, new NodeRule("value", CommonLang.createBlockRule(List.of(
-                new TypeRule(CommonLang.DECLARATION_TYPE, new PrefixRule("let x : I32 = 420;", new EmptyRule())),
-                CommonLang.createReturnRule()
-        ))));
+        return new EmptyRule();
     }
 }
