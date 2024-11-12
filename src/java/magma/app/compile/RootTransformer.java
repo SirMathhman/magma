ef563b1580d15f520677d2ae1f4b3a2bdde23bb4
SirMathhman <sirmathhman@gmail.com>
2024-11-12 16:23:00 -0500

    Nuke

--- src/java/magma/app/compile/RootTransformer.java
+++ src/java/magma/app/compile/RootTransformer.java
@@ -13,12 +13,9 @@ import magma.java.JavaList;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
 
 import static magma.Assembler.STACK_POINTER;
 import static magma.app.compile.lang.CASMLang.*;
-import static magma.app.compile.lang.MagmaLang.NUMBER_TYPE;
-import static magma.app.compile.lang.MagmaLang.NUMBER_VALUE;
 import static magma.app.compile.lang.MagmaLang.*;
 
 class RootTransformer implements Passer {
@@ -78,48 +75,5 @@ class RootTransformer implements Passer {
 
     private JavaList<Node> foldRootChild(JavaList<Node> current, Node node) {
         return current;
-    }
-
-    private Result<List<Node>, CompileError> resolveValue(Node value, List<Tuple<String, Integer>> labels) {
-        if (value.is(NUMBER_TYPE)) {
-            final var numericValueOption = value.findInt(NUMBER_VALUE);
-            if (numericValueOption.isEmpty())
-                return new Err<>(new CompileError("No numeric value present", new NodeContext(value)));
-
-            final var numericValue = numericValueOption.orElse(0);
-            return new Ok<>(List.of(
-                    new MapNode("instruction").withString(MNEMONIC, "ldv").withInt(ADDRESS_OR_VALUE, numericValue)
-            ));
-        } else if (value.is(SYMBOL_TYPE)) {
-            final var valueOption = value.findString(SYMBOL_VALUE);
-            if (valueOption.isEmpty()) return new Err<>(new CompileError("No value in symbol", new NodeContext(value)));
-            final var name = valueOption.orElse("");
-
-            final var indexOptional = IntStream.range(0, labels.size())
-                    .filter(maybeIndex -> labels.get(maybeIndex).left().equals(name))
-                    .findFirst();
-
-            if (indexOptional.isEmpty()) {
-                return new Err<>(new CompileError("Symbol not defined - " + name, new NodeContext(value)));
-            }
-
-            final var startingIndex = indexOptional.getAsInt();
-            var bytesTotal = 0;
-            for (int i = 0; i < startingIndex; i++) {
-                bytesTotal += labels.get(i).right();
-            }
-
-            final var list = List.of(
-                    new MapNode("instruction").withString(MNEMONIC, "ldd").withString(INSTRUCTION_LABEL, STACK_POINTER),
-                    new MapNode("instruction").withString(MNEMONIC, "addv").withInt(ADDRESS_OR_VALUE, bytesTotal),
-                    new MapNode("instruction").withString(MNEMONIC, "stod").withString(INSTRUCTION_LABEL, STACK_POINTER),
-                    new MapNode("instruction").withString(MNEMONIC, "ldi").withString(INSTRUCTION_LABEL, STACK_POINTER),
-                    new MapNode("instruction").withString(MNEMONIC, "stod").withString(INSTRUCTION_LABEL, "temp")
-            );
-
-            return new Ok<>(list);
-        } else {
-            return new Err<>(new CompileError("Unknown type", new NodeContext(value)));
-        }
     }
 }
