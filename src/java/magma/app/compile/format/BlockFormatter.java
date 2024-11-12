d72a12710bb4dbca337439bda1fabdbd1aba2104
SirMathhman <sirmathhman@gmail.com>
2024-11-12 08:49:20 -0500

    Formatting ok!

--- src/java/magma/app/compile/format/BlockFormatter.java
+++ src/java/magma/app/compile/format/BlockFormatter.java
@@ -16,9 +16,9 @@ import java.util.List;
 import static magma.app.compile.lang.CASMLang.*;
 
 public class BlockFormatter implements Passer {
-    private static Result<List<Node>, CompileError> attachPadding(List<Node> list) {
+    private static Result<List<Node>, CompileError> attachPadding(State state, List<Node> list) {
         return new Ok<>(list.stream()
-                .map(child -> child.withString(BLOCK_BEFORE_CHILD, "\n\t"))
+                .map(child -> child.withString(BLOCK_BEFORE_CHILD, computeIndent(state, 0)))
                 .toList());
     }
 
@@ -33,8 +33,13 @@ public class BlockFormatter implements Passer {
     public Option<Result<Tuple<State, Node>, CompileError>> afterPass(State state, Node node) {
         if (!node.is(BLOCK_TYPE)) return new None<>();
 
-        return node.withString(BLOCK_AFTER_CHILDREN, "\n" + "\t".repeat(state.depth()))
-                .mapNodeList(CHILDREN, BlockFormatter::attachPadding)
+        return node.withString(BLOCK_AFTER_CHILDREN, computeIndent(state, 1))
+                .mapNodeList(CHILDREN, list -> attachPadding(state, list))
                 .map(result -> result.mapValue(value -> new Tuple<>(state.exit(), value)));
+    }
+
+    private static String computeIndent(State state, int offset) {
+        final var indentValue = state.depth() - offset;
+        return "\n" + "\t".repeat(Math.max(0, indentValue));
     }
 }
