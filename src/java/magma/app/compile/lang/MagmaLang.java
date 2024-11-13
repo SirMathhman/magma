18e0756756d89b0267b0d101d0ff272a47b64fe3
SirMathhman <sirmathhman@gmail.com>
2024-11-13 09:02:13 -0500

    Return OK

--- src/java/magma/app/compile/lang/MagmaLang.java
+++ src/java/magma/app/compile/lang/MagmaLang.java
@@ -1,10 +1,11 @@
 package magma.app.compile.lang;
 
 import magma.app.compile.rule.EmptyRule;
+import magma.app.compile.rule.PrefixRule;
 import magma.app.compile.rule.Rule;
 
 public class MagmaLang {
     public static Rule createMagmaRootRule() {
-        return new EmptyRule();
+        return new PrefixRule("return;", new EmptyRule());
     }
 }
