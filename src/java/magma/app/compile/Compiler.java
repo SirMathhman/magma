b8efd7929383596b7514820bfbd066a06c838f9a
SirMathhman <sirmathhman@gmail.com>
2024-11-13 08:39:07 -0500

    ???

--- src/java/magma/app/compile/Compiler.java
+++ src/java/magma/app/compile/Compiler.java
@@ -11,6 +11,7 @@ import java.util.List;
 public record Compiler(String input) {
     private static CompoundPassingStage createPassingStage() {
         return new CompoundPassingStage(List.of(
+                
         ));
     }
 
