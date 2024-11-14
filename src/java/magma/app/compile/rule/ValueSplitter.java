6262c281b937ed62039bc79c0602c49ec55e4e48
SirMathhman <sirmathhman@gmail.com>
2024-11-14 12:16:20 -0500

    Array in array OK

--- src/java/magma/app/compile/rule/ValueSplitter.java
+++ src/java/magma/app/compile/rule/ValueSplitter.java
@@ -1,11 +1,28 @@
 package magma.app.compile.rule;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ValueSplitter implements Splitter {
     @Override
     public List<String> split(String input) {
-        return Arrays.stream(input.split(",")).toList();
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        var length = input.length();
+        for (int i = 0; i < length; i++) {
+            var c = input.charAt(i);
+            if (c == ',' && depth == 0) {
+                if (!buffer.isEmpty()) segments.add(buffer.toString());
+                buffer = new StringBuilder();
+            } else {
+                buffer.append(c);
+                if (c == '[') depth++;
+                if (c == ']') depth--;
+            }
+        }
+
+        if (!buffer.isEmpty()) segments.add(buffer.toString());
+        return segments;
     }
 }
