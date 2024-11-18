80c0691d180fd199c6e54442adce78f85d257c56
SirMathhman <sirmathhman@gmail.com>
2024-11-18 10:35:42 -0500

    ...

--- src/java/magma/ApplicationError.java
+++
@@ -1,4 +0,0 @@
-package magma;
-
-public class ApplicationError {
-}
--- src/java/magma/Result.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-import java.util.function.Function;
-
-public interface Result<T, E> {
-    <R> R match(Function<T, R> onOk, Function<E, R> onErr);
-
-    boolean isErr();
-}
