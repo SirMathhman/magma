d34d6cea59441e41056216aafb5c71208d0d8d12
SirMathhman <sirmathhman@gmail.com>
2024-11-18 10:21:46 -0500

    Packaging

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,7 +1,5 @@
-import magma.ApplicationError;
-import magma.Err;
-import magma.Ok;
-import magma.Result;
+package magma;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
