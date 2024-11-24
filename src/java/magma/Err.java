0539122cc05b43272c75c22b4fde215f1a3196d4
SirMathhman <sirmathhman@gmail.com>
2024-11-23 21:13:50 -0500

    Implement missing

--- src/java/magma/Err.java
+++ src/java/magma/Err.java
@@ -1,4 +1,26 @@
 package magma;
 
-public record Err<T, X>(X invalidRoot) implements Result<T, X> {
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public record Err<T, X>(X error) implements Result<T, X> {
+    @Override
+    public <R> Result<Tuple<T, R>, X> and(Supplier<Result<R, X>> supplier) {
+        return new Err<>(error);
+    }
+
+    @Override
+    public <R> Result<R, X> mapValue(Function<T, R> mapper) {
+        return new Err<>(error);
+    }
+
+    @Override
+    public <R> Result<T, R> mapErr(Function<X, R> mapper) {
+        return new Err<>(mapper.apply(error));
+    }
+
+    @Override
+    public <R> R match(Function<T, R> onOk, Function<X, R> onErr) {
+        return onErr.apply(error);
+    }
 }
--- src/java/magma/Some.java
+++ src/java/magma/Some.java
@@ -1,4 +1,16 @@
 package magma;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public record Some<T>(T value) implements Option<T> {
+    @Override
+    public <R> Option<R> map(Function<T, R> mapper) {
+        return new Some<>(mapper.apply(value));
+    }
+
+    @Override
+    public T orElseGet(Supplier<T> supplier) {
+        return value;
+    }
 }
