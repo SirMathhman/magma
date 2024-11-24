9c63659e84b7f7524a929f4f4af77759ed16a5e4
SirMathhman <sirmathhman@gmail.com>
2024-11-23 20:56:52 -0500

    Cleanup Main

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,36 +4,47 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
+    public static final Path SOURCE_DIRECTORY = Paths.get(".", "src", "java");
+    public static final Path TARGET_DIRECTORY = Paths.get(".", "src", "magma");
+
     public static void main(String[] args) {
-        final var sourceDirectory = Paths.get(".", "src", "java");
-        final java.util.Set<Path> sources;
-        try (var stream = Files.walk(sourceDirectory)) {
-            sources = stream
-                    .filter(Files::isRegularFile)
-                    .filter(file -> file.getFileName().toString().endsWith(".java"))
-                    .collect(Collectors.toSet());
+        final var sources = collectSources();
+        compileSources(sources);
+    }
+
+    private static void compileSources(Set<Path> sources) {
+        try {
+            for (var source : sources) {
+                compileSource(source);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        try {
-            final var targetDirectory = Paths.get(".", "src", "magma");
-            for (var source : sources) {
-                final var relativized = sourceDirectory.relativize(source);
-                final var nameWithExt = relativized.getFileName().toString();
-                final var name = nameWithExt.substring(0, nameWithExt.indexOf('.'));
+    private static void compileSource(Path source) throws IOException {
+        final var relativized = SOURCE_DIRECTORY.relativize(source);
+        final var nameWithExt = relativized.getFileName().toString();
+        final var name = nameWithExt.substring(0, nameWithExt.indexOf('.'));
 
-                final var targetParent = targetDirectory.resolve(relativized.getParent());
+        final var targetParent = TARGET_DIRECTORY.resolve(relativized.getParent());
 
-                if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
-                final var target = targetParent.resolve(name + ".mgs");
+        if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
+        final var target = targetParent.resolve(name + ".mgs");
 
-                final var input = Files.readString(source);
-                Files.writeString(target, input);
-            }
+        final var input = Files.readString(source);
+        Files.writeString(target, input);
+    }
+
+    private static Set<Path> collectSources() {
+        try (var stream = Files.walk(SOURCE_DIRECTORY)) {
+            return stream.filter(Files::isRegularFile)
+                    .filter(file -> file.getFileName().toString().endsWith(".java"))
+                    .collect(Collectors.toSet());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
