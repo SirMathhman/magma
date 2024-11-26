13b06ff57a78cb6b34150f426697b4a93db9151f
SirMathhman <sirmathhman@gmail.com>
2024-11-26 17:25:56 -0500

    Nuke again

--- src/java/magma/error/JavaError.java
+++
@@ -1,8 +0,0 @@
-package magma.error;
-
-public record JavaError<X extends Exception>(X error) implements Error {
-    @Override
-    public String display() {
-        return error.getMessage();
-    }
-}
--- src/java/magma/error/StringContext.java
+++
@@ -1,8 +0,0 @@
-package magma.error;
-
-public record StringContext(String context) implements Context {
-    @Override
-    public String format() {
-        return context();
-    }
-}
