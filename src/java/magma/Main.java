5d40de4b7a41535301e658d2ce28556dabb2dc15
SirMathhman <sirmathhman@gmail.com>
2024-11-29 16:32:44 -0500

    Fix immutable design

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -37,7 +37,7 @@ public class Main {
 
     public static void main(String[] args) {
         final var source = """
-                out 3;
+                out 'a';
                 """;
 
         compile(source)
--- src/java/magma/app/compile/rule/OrRule.java
+++ src/java/magma/app/compile/rule/OrRule.java
@@ -1,13 +1,13 @@
 package magma.app.compile.rule;
 
-import magma.app.compile.Node;
+import magma.api.result.Err;
+import magma.api.result.Result;
 import magma.app.compile.CompileError;
+import magma.app.compile.Node;
 import magma.app.error.Error;
 import magma.app.error.NodeContext;
 import magma.app.error.StringContext;
 import magma.java.JavaList;
-import magma.api.result.Err;
-import magma.api.result.Result;
 
 public record OrRule(JavaList<Rule> rules) implements Rule {
     @Override
@@ -16,7 +16,7 @@ public record OrRule(JavaList<Rule> rules) implements Rule {
         for (var rule : rules.list()) {
             final var parsed = rule.parse(input);
             if (parsed.isOk()) return parsed;
-            causes.add(parsed.findErr().orElse(new CompileError()));
+            causes = causes.add(parsed.findErr().orElse(new CompileError()));
         }
         return new Err<>(new CompileError("No valid rule present", new StringContext(input), causes));
     }
@@ -27,7 +27,7 @@ public record OrRule(JavaList<Rule> rules) implements Rule {
         for (var rule : rules.list()) {
             final var generated = rule.generate(node);
             if (generated.isOk()) return generated;
-            causes.add(generated.findErr().orElse(new CompileError()));
+            causes = causes.add(generated.findErr().orElse(new CompileError()));
         }
         return new Err<>(new CompileError("No valid rule present", new NodeContext(node), causes));
     }
