3226b8d18eb6f9e918cb5090dff386262781d8cd
SirMathhman <sirmathhman@gmail.com>
2024-12-01 14:36:43 -0500

    OK

--- src/java/magma/app/compile/lang/magma/FlattenAssignment.java
+++ src/java/magma/app/compile/lang/magma/FlattenAssignment.java
@@ -8,8 +8,6 @@ import magma.api.result.Result;
 import magma.app.compile.MapNode;
 import magma.app.compile.Node;
 import magma.app.compile.error.CompileError;
-import magma.app.compile.lang.common.CommonLang;
-import magma.java.JavaList;
 
 public class FlattenAssignment implements magma.app.compile.pass.Passer {
     @Override
@@ -19,8 +17,8 @@ public class FlattenAssignment implements magma.app.compile.pass.Passer {
         final var variable = node.findNode(MagmaLang.ASSIGNMENT_VARIABLE).orElse(new MapNode());
         final var expression = node.findNode(MagmaLang.ASSIGNMENT_EXPRESSION).orElse(new MapNode());
 
-        return new Some<>(new Ok<>(CommonLang.toGroup(new JavaList<Node>()
-                .add(new MapNode("move").withNode("value", variable))
-                .add(new MapNode("store").withNode("value", expression)))));
+        return new Some<>(new Ok<>(new MapNode("move")
+                .withNode("destination", variable)
+                .withNode("source", expression)));
     }
 }
