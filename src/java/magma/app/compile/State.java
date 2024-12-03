407f7dd6bc00c70d8a2c46ac78aef4368179cb36
SirMathhman <sirmathhman@gmail.com>
2024-12-02 19:19:35 -0500

    Fix offset

--- src/java/magma/app/compile/State.java
+++ src/java/magma/app/compile/State.java
@@ -2,61 +2,32 @@ package magma.app.compile;
 
 import magma.api.Tuple;
 import magma.api.option.Option;
-import magma.api.stream.Stream;
-import magma.api.stream.Streams;
-import magma.java.JavaNonEmptyList;
 import magma.java.JavaOrderedMap;
 
-public record State(JavaNonEmptyList<JavaOrderedMap<String, Node>> frames) {
+public record State(Frame frame) {
     public State() {
-        this(new JavaNonEmptyList<>(new JavaOrderedMap<>()));
+        this(new Frame());
     }
 
     public State define(String name, Node type) {
-        return new State(frames.mapLast(last -> last.put(name, type)));
-    }
-
-    public Option<Node> resolve(String name) {
-        return resolveIndexAndType(name)
-                .map(Tuple::right)
-                .map(Tuple::right);
-    }
-
-    private Option<Tuple<Integer, Tuple<Integer, Node>>> resolveIndexAndType(String name) {
-        return frames.streamReverseWithIndices()
-                .map(frame -> frame.right().findIndexAndValue(name).map(inner -> new Tuple<>(frame.left(), inner)))
-                .flatMap(Streams::fromOption)
-                .next();
+        return new State(frame.define(name, type));
     }
 
     public Option<Integer> computeOffset(String name) {
-        return resolveIndexAndType(name).map(tuple -> {
-            final var other = tuple.right();
-
-            final var requestedFrameIndex = tuple.left();
-            final var requestedElementIndex = other.left();
-
-            final var currentFrameIndex = frames.size() - 1;
-            final var currentElementIndex = frames.last().size() - 1;
-
-            if (requestedFrameIndex == currentFrameIndex &&
-                    requestedElementIndex == currentElementIndex) return 0;
-
-            else {
-                throw new UnsupportedOperationException();
-            }
-        });
+        return frame.countBefore(name);
     }
 
-    private int computeSizeFromFrameIndex(int frameIndex) {
-        return frames.sliceFrom(frameIndex)
-                .map(slice -> slice.map(frame -> computeFrameSize(frame.stream())).foldLeft(0, Integer::sum))
-                .orElse(0);
-    }
+    record Frame(JavaOrderedMap<String, Node> frame) {
+        public Frame() {
+            this(new JavaOrderedMap<>());
+        }
 
-    private int computeFrameSize(Stream<Tuple<String, Node>> frameStream) {
-        return frameStream.map(Tuple::right)
-                .map(right -> right.findInt("length").orElse(0))
-                .foldLeft(0, Integer::sum);
+        public Frame define(String name, Node type) {
+            return new Frame(frame.put(name, type));
+        }
+
+        public Option<Integer> countBefore(String name) {
+            return frame.findIndexAndValue(name).map(Tuple::left);
+        }
     }
 }
