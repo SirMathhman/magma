56afd6a5c98069707b440a05b5cbb0eea47efd3d
SirMathhman <sirmathhman@gmail.com>
2024-12-02 19:27:21 -0500

    Fix sign issues

--- src/java/magma/app/compile/lang/casm/ExpandMoveStackPointer.java
+++ src/java/magma/app/compile/lang/casm/ExpandMoveStackPointer.java
@@ -1,14 +1,13 @@
 package magma.app.compile.lang.casm;
 
 import magma.app.compile.Node;
-import magma.app.compile.lang.casm.assemble.Operator;
 import magma.app.compile.lang.common.CommonLang;
 import magma.app.compile.lang.magma.Stateless;
 import magma.java.JavaList;
 
 import static magma.app.compile.Compiler.SPILL;
 import static magma.app.compile.Compiler.STACK_POINTER;
-import static magma.app.compile.lang.casm.CASMLang.*;
+import static magma.app.compile.lang.casm.CASMLang.instruct;
 import static magma.app.compile.lang.casm.assemble.Operator.*;
 
 public class ExpandMoveStackPointer implements Stateless {
@@ -16,10 +15,10 @@ public class ExpandMoveStackPointer implements Stateless {
     public Node beforePass(Node node) {
         final var offset = node.findInt("offset").orElse(0);
 
-        if(offset == 0) return CommonLang.createEmptyGroup();
+        if (offset == 0) return CommonLang.createEmptyGroup();
 
         var instruction = offset < 0
-                ? instruct(SubtractFromValue, offset)
+                ? instruct(SubtractFromValue, -offset)
                 : instruct(AddFromValue, offset);
 
         return CommonLang.asGroup(new JavaList<Node>()
