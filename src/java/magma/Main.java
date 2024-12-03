717d30c71c1801098553af862bf928eb51b9b187
SirMathhman <sirmathhman@gmail.com>
2024-12-03 16:43:54 -0500

    Package statements

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -16,16 +18,79 @@ public class Main {
                     .filter(path -> path.toString().endsWith(".java"))
                     .collect(Collectors.toSet());
 
-            for (Path source : sources) {
-                final var fileName = source.getFileName().toString();
-                final var separator = fileName.indexOf('.');
-                final var name = fileName.substring(0, separator);
-
-                final var input = Files.readString(source);
-                Files.writeString(source.resolveSibling(name + ".mgs"), input);
-            }
-        } catch (IOException e) {
+            runWithSources(sources);
+        } catch (IOException | CompileException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void runWithSources(Set<Path> sources) throws IOException, CompileException {
+        for (Path source : sources) {
+            final var fileName = source.getFileName().toString();
+            final var separator = fileName.indexOf('.');
+            final var name = fileName.substring(0, separator);
+            final var target = source.resolveSibling(name + ".mgs");
+
+            final var input = Files.readString(source);
+            final var output = compile(input);
+            Files.writeString(target, output);
+        }
+    }
+
+    private static String compile(String input) throws CompileException {
+        final var segments = split(input);
+
+        var output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootMember(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static List<String> split(String input) {
+        var state = new State();
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            var appended = state.append(c);
+            state = splitAtChar(c, appended);
+        }
+
+        return state.advance().segments;
+    }
+
+    private static State splitAtChar(char c, State appended) {
+        if (c == ';') {
+            return appended.advance();
+        } else {
+            return appended;
+        }
+    }
+
+    private static String compileRootMember(String input) throws CompileException {
+        if (input.startsWith("package ")) {
+            if (input.endsWith(";")) {
+                return "";
+            }
+        }
+
+        throw new CompileException(input);
+    }
+
+    private record State(List<String> segments, StringBuilder buffer) {
+        public State() {
+            this(new ArrayList<>(), new StringBuilder());
+        }
+
+        private State append(char c) {
+            return new State(segments, buffer.append(c));
+        }
+
+        private State advance() {
+            if (buffer.isEmpty()) return this;
+            final var copy = new ArrayList<>(segments);
+            copy.add(buffer.toString());
+            return new State(copy, new StringBuilder());
         }
     }
 }
