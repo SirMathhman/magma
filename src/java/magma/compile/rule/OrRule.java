9b4cda8e5013a387f26c7ff5e4407294bbe32157
SirMathhman <sirmathhman@gmail.com>
2024-12-04 11:59:38 -0500

    Cleanup OrRule with more detailed errors

--- src/java/magma/compile/rule/OrRule.java
+++ src/java/magma/compile/rule/OrRule.java
@@ -9,47 +9,46 @@ import magma.api.result.Result;
 import magma.api.stream.Streams;
 import magma.compile.Node;
 import magma.compile.error.CompileError;
+import magma.compile.error.Context;
 import magma.compile.error.NodeContext;
 import magma.compile.error.StringContext;
 import magma.java.JavaList;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public record OrRule(List<Rule> rules) implements Rule {
     @Override
     public Result<Node, CompileError> parse(String input) {
         return Streams.from(rules)
-                .foldLeft(new State(), (state, rule) -> state.foldCombination(rule, input))
-                .complete(input);
+                .foldLeft(new Accumulator<Node>(), (accumulator, rule) -> accumulator.foldCombination(() -> rule.parse(input)))
+                .complete(() -> new StringContext(input));
     }
 
     @Override
     public Result<String, CompileError> generate(Node node) {
-        for (Rule rule : rules) {
-            final var generated = rule.generate(node);
-            if (generated.isOk()) return generated;
-        }
-
-        return new Err<>(new CompileError("No valid combination", new NodeContext(node)));
+        return Streams.from(rules)
+                .foldLeft(new Accumulator<String>(), (accumulator, rule) -> accumulator.foldCombination(() -> rule.generate(node)))
+                .complete(() -> new NodeContext(node));
     }
 
-    private record State(Option<Node> maybePreviousNode, JavaList<CompileError> previousErrors) {
-        public State() {
+    private record Accumulator<T>(Option<T> maybePreviousValue, JavaList<CompileError> previousErrors) {
+        public Accumulator() {
             this(new None<>(), new JavaList<>());
         }
 
-        private State foldCombination(Rule rule, String input) {
-            if (maybePreviousNode.isPresent()) return this;
+        private Accumulator<T> foldCombination(Supplier<Result<T, CompileError>> compute) {
+            if (maybePreviousValue.isPresent()) return this;
 
-            return rule.parse(input).match(
-                    node -> new State(new Some<>(node), new JavaList<>()),
-                    error -> new State(new None<>(), previousErrors.add(error))
+            return compute.get().match(
+                    value -> new Accumulator<>(new Some<>(value), new JavaList<>()),
+                    error -> new Accumulator<>(new None<>(), previousErrors.add(error))
             );
         }
 
-        public Result<Node, CompileError> complete(String input) {
-            return maybePreviousNode.<Result<Node, CompileError>>map(Ok::new).orElseGet(() -> {
-                final var context = new StringContext(input);
+        public Result<T, CompileError> complete(Supplier<Context> contextFactory) {
+            return maybePreviousValue.<Result<T, CompileError>>map(Ok::new).orElseGet(() -> {
+                final var context = contextFactory.get();
                 final var error = new CompileError("No valid combination", context, previousErrors);
                 return new Err<>(error);
             });
