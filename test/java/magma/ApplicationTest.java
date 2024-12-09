45a13b4e43297a196b1ac31eb4e599b32eddefca
SirMathhman <sirmathhman@gmail.com>
2024-12-09 16:29:18 -0500

    Return statements

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,21 +1,37 @@
 package magma;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+    public static final String RETURN_KEYWORD_WITH_SPACE = "return ";
+    public static final String STATEMENT_END = ";";
+
+    private static String run(String input) {
+        if (input.startsWith(RETURN_KEYWORD_WITH_SPACE) && input.endsWith(STATEMENT_END)) {
+            return input.substring(RETURN_KEYWORD_WITH_SPACE.length(), input.length() - STATEMENT_END.length());
+        }
+
+        return input;
+    }
+
     @Test
     void empty() {
         assertEquals("", run(""));
     }
 
-    @Test
-    void value() {
-        assertEquals("100", run("100"));
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "200"})
+    void value(String value) {
+        assertEquals(value, run(value));
     }
 
-    private static String run(String input) {
-        return input;
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "200"})
+    void returns(String value) {
+        assertEquals(value, run(RETURN_KEYWORD_WITH_SPACE + value + STATEMENT_END));
     }
 }
