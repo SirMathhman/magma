4152fb39a9c30d7db8f4ab859290275ffbc31853
SirMathhman <sirmathhman@gmail.com>
2024-12-09 10:29:34 -0500

    Move into Label

--- src/java/magma/Label.java
+++ src/java/magma/Label.java
@@ -1,6 +1,16 @@
 package magma;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record Label(List<Instruction> instructions) {
+    public Label() {
+        this(new ArrayList<>());
+    }
+
+    Label instruct(List<Instruction> instructions) {
+        final var copy = new ArrayList<>(this.instructions);
+        copy.addAll(instructions);
+        return new Label(copy);
+    }
 }
--- src/java/magma/State.java
+++ src/java/magma/State.java
@@ -1,9 +1,7 @@
 package magma;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -19,7 +17,7 @@ final class State {
     }
 
     public State() {
-        this(new Stack(), new Label(new ArrayList<>()));
+        this(new Stack(), new Label());
     }
 
     public State enter() {
@@ -32,54 +30,26 @@ final class State {
 
     public State defineData(String name, long size, Function<Stack, List<Instruction>> loader) {
         final var withA = stack.define(name, size);
-        final var instructions = assign(name, loader);
-        return new State(withA, new Label(instructions));
+        final var label1 = label.instruct(createAssignmentInstructions(name, loader));
+        return new State(withA, label1);
+    }
+
+    private List<Instruction> createAssignmentInstructions(String name, Function<Stack, List<Instruction>> loader) {
+        return Stream.of(loader.apply(stack), List.of(StoreDirect.of(new DataAddress(stack.resolveAddress(name)))))
+                .flatMap(Collection::stream)
+                .toList();
     }
 
     public State assignAsState(String name, Function<Stack, List<Instruction>> loader) {
-        final var instructions = assign(name, loader);
-        return new State(stack, new Label(instructions));
-    }
-
-    private List<Instruction> assign(String name, Function<Stack, List<Instruction>> loader) {
-        final var instructions = Stream.of(loader.apply(stack), List.of(StoreDirect.of(new DataAddress(stack.resolveAddress(name)))))
-                .flatMap(Collection::stream)
-                .toList();
-
-        final var copy = new ArrayList<>(label.instructions());
-        copy.addAll(instructions);
-        return copy;
+        final var label1 = label.instruct(createAssignmentInstructions(name, loader));
+        return new State(stack, label1);
     }
 
     public State defineData(String name, long size) {
         return new State(stack.define(name, size), label);
     }
 
-    public Stack stack() {
-        return stack;
-    }
-
     public List<Instruction> instructions() {
         return label.instructions();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) return true;
-        if (obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (State) obj;
-        return Objects.equals(this.stack, that.stack) && Objects.equals(label.instructions(), that.label.instructions());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(stack, label.instructions());
-    }
-
-    @Override
-    public String toString() {
-        return "State[" +
-                "stack=" + stack + ", " +
-                "instructions=" + label.instructions() + ']';
     }
 }
