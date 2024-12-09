15b50b18e6ca8c298e3dac95c2549cca90b1c767
SirMathhman <sirmathhman@gmail.com>
2024-12-09 16:23:21 -0500

    Nuke

--- src/java/magma/Tuple.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-import java.util.function.Function;
-
-public record Tuple<A, B>(A left, B right) {
-    public <R> Tuple<A, R> mapRight(Function<B, R> mapper) {
-        return new Tuple<>(left, mapper.apply(right));
-    }
-}
