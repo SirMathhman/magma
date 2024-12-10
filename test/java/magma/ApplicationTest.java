743ff4248bcffb4a7f0b7168495675f79ef69e3b
SirMathhman <sirmathhman@gmail.com>
2024-12-10 08:18:16 -0500

    Numeric values

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,12 +1,24 @@
 package magma;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
     @Test
     void empty() {
-        assertEquals("", "");
+        assertEquals("", run(""));
+    }
+    
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "200"})
+    void numeric(String value) {
+        assertEquals(value, run(value));
+    }
+
+    private static String run(String value) {
+        return value;
     }
 }
