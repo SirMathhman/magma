622377944ea2495e85fec41f218a341dd27b3811
SirMathhman <sirmathhman@gmail.com>
2024-12-09 20:10:21 -0500

    Empty

---
+++ test/java/magma/ApplicationTest.java
@@ -0,0 +1,11 @@
+package magma;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    @Test
+    void empty() {
+        Assertions.assertEquals("", "");
+    }
+}
