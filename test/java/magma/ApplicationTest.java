108dcaff40a56ff9d4d58efb75dcbd47f5ead9d6
SirMathhman <sirmathhman@gmail.com>
2024-12-09 20:12:14 -0500

    Returns

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -7,18 +7,34 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApplicationTest {
+    @Test
+    void returns() {
+        assertRun(generateReturn(), "10");
+    }
+
+    private static String generateReturn() {
+        return "return 10;";
+    }
+
     private static String run(String input) {
+        if(input.equals(generateReturn())) {
+            return "10";
+        }
         return input;
     }
 
     @Test
     void empty() {
-        assertEquals("", run(""));
+        assertRun("", "");
+    }
+
+    private static void assertRun(String input, String output) {
+        assertEquals(output, run(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"100", "200"})
     void value(String value) {
-        assertEquals(value, run(value));
+        assertRun(value, value);
     }
 }
