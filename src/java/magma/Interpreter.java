8004e23208e47f70a3b9ef93683eee22cd61ab25
SirMathhman <sirmathhman@gmail.com>
2024-12-11 10:49:50 -0500

    Interpreter

---
+++ src/java/magma/Interpreter.java
@@ -0,0 +1,74 @@
+package magma;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class Interpreter {
+    static Result<State, Tuple<State, RuntimeError>> interpreter(Deque<Integer> input) {
+        var memory = new ArrayList<Integer>();
+        memory.add(InStore.of(1));
+
+        var programCounter = 0;
+
+        var state = new State(input, memory, programCounter);
+        while (true) {
+            final var optional = cycle(state);
+            if (optional.isPresent()) {
+                final var result = optional.get();
+                final var value = result.findValue();
+                if (value.isPresent()) {
+                    state = value.orElseThrow();
+                }
+
+                final var error = result.findError();
+                if (error.isPresent()) {
+                    return new Err<State, Tuple<State, RuntimeError>>(new Tuple<State, RuntimeError>(state, error.orElseThrow()));
+                }
+            } else {
+                break;
+            }
+        }
+
+        return new Ok<State, Tuple<State, RuntimeError>>(state);
+    }
+
+    static Optional<Result<State, RuntimeError>> cycle(State state) {
+        return state.current()
+                .map(Main::decode)
+                .flatMap(result -> processDecoded(state, result));
+    }
+
+    static Optional<Result<State, RuntimeError>> processDecoded(State state, Result<Instruction, RuntimeError> result) {
+        return result.mapValue(instruction -> processInstruction(state, instruction))
+                .into(Results::invertOption)
+                .map(inner -> inner.flatMapValue(Function.identity()));
+    }
+
+    static Optional<Result<State, RuntimeError>> processInstruction(State state, Instruction instruction) {
+        final var next = state.next();
+        final var operation = instruction.operation();
+        final var addressOrValue = instruction.addressOrValue();
+        return switch (operation) {
+            case Nothing -> Optional.of(new Ok<State, RuntimeError>(next));
+            case InStore -> Optional.of(new Ok<State, RuntimeError>(next.inAndStore(addressOrValue)));
+            case Halt -> Optional.empty();
+            case Jump -> Optional.of(new Ok<State, RuntimeError>(next.jump(addressOrValue)));
+            case LoadValue -> Optional.of(new Ok<State, RuntimeError>(next.loadValue(addressOrValue)));
+            case LoadDirect -> Optional.of(next.loadDirect(addressOrValue));
+            case StoreIndirect -> Optional.of(new Ok<State, RuntimeError>(next.storeIndirect(addressOrValue)));
+            case StoreDirect -> Optional.of(new Ok<State, RuntimeError>(next.storeDirect(addressOrValue)));
+            case AddValue -> Optional.of(new Ok<State, RuntimeError>(next.addValue(addressOrValue)));
+            case AddAddress -> Optional.of(new Ok<State, RuntimeError>(next.addAddress(addressOrValue)));
+            case SubtractValue -> Optional.of(new Ok<State, RuntimeError>(next.subtractValue(addressOrValue)));
+            case LoadIndirect -> Optional.of(next.loadIndirect(addressOrValue));
+        };
+    }
+
+    static Result<Instruction, RuntimeError> decode(Integer instruction1) {
+        return Instruction.decode(instruction1)
+                .<Result<Instruction, RuntimeError>>map(Ok::new)
+                .orElseGet(() -> new Err<Instruction, RuntimeError>(new RuntimeError("Unknown instruction", Integer.toHexString(instruction1))));
+    }
+}
--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,15 +1,16 @@
 package magma;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static magma.Operation.*;
+import static magma.Operation.InStore;
+import static magma.Operation.Jump;
 
 public class Main {
 
@@ -19,7 +20,7 @@ public class Main {
     public static void main(String[] args) {
         var input = createLoadableProgram().collect(Collectors.toCollection(LinkedList::new));
 
-        run(input).consume(state -> {
+        Interpreter.interpreter(input).consume(state -> {
             final var joined = state.display();
             System.out.println(joined);
         }, err -> {
@@ -29,34 +30,6 @@ public class Main {
             System.err.println(error.display());
             System.err.println(state.display());
         });
-    }
-
-    private static Result<State, Tuple<State, RuntimeError>> run(LinkedList<Integer> input) {
-        var memory = new ArrayList<Integer>();
-        memory.add(InStore.of(1));
-
-        var programCounter = 0;
-
-        var state = new State(input, memory, programCounter);
-        while (true) {
-            final var optional = cycle(state);
-            if (optional.isPresent()) {
-                final var result = optional.get();
-                final var value = result.findValue();
-                if (value.isPresent()) {
-                    state = value.orElseThrow();
-                }
-
-                final var error = result.findError();
-                if (error.isPresent()) {
-                    return new Err<>(new Tuple<>(state, error.orElseThrow()));
-                }
-            } else {
-                break;
-            }
-        }
-
-        return new Ok<>(state);
     }
 
     private static Stream<Integer> createLoadableProgram() {
@@ -77,53 +50,7 @@ public class Main {
     }
 
     private static List<Integer> createProgram() {
-        return Stream.of(
-                define(1, loadValue(0x100)),
-                define(2, Stream.of(
-                        loadOffset(1)
-                ).flatMap(Function.identity())),
-                Stream.of(Halt.empty())
-        ).flatMap(Function.identity()).collect(Collectors.toCollection(ArrayList::new));
-    }
-
-    private static Stream<Integer> loadValue(int value) {
-        return Stream.of(LoadValue.of(value));
-    }
-
-    private static Stream<Integer> loadOffset(int offset) {
-        return Stream.of(
-                moveToOffset(offset),
-                Stream.of(LoadIndirect.of(STACK_POINTER)),
-                moveToOffset(-offset)
-        ).flatMap(Function.identity());
-    }
-
-    private static Stream<Integer> define(int offset, Stream<Integer> loader) {
-        return Stream.of(
-                loader,
-                Stream.of(
-                        Stream.of(StoreDirect.of(SPILL)),
-                        moveToOffset(offset),
-                        Stream.of(
-                                LoadDirect.of(SPILL),
-                                StoreIndirect.of(STACK_POINTER)
-                        ),
-                        moveToOffset(-offset)
-                ).flatMap(Function.identity())
-        ).flatMap(Function.identity());
-    }
-
-    private static Stream<Integer> moveToOffset(int offset) {
-        if (offset == 0) return Stream.empty();
-        var instruction = offset > 0
-                ? AddValue.of(offset)
-                : SubtractValue.of(-offset);
-
-        return Stream.of(
-                LoadDirect.of(STACK_POINTER),
-                instruction,
-                StoreDirect.of(STACK_POINTER)
-        );
+        return Collections.emptyList();
     }
 
     private static Stream<Integer> set(int address, int instruction) {
@@ -131,43 +58,5 @@ public class Main {
                 InStore.of(address),
                 instruction
         );
-    }
-
-    private static Optional<Result<State, RuntimeError>> cycle(State state) {
-        return state.current()
-                .map(Main::decode)
-                .flatMap(result -> processDecoded(state, result));
-    }
-
-    private static Optional<Result<State, RuntimeError>> processDecoded(State state, Result<Instruction, RuntimeError> result) {
-        return result.mapValue(instruction -> processInstruction(state, instruction))
-                .into(Results::invertOption)
-                .map(inner -> inner.flatMapValue(Function.identity()));
-    }
-
-    private static Optional<Result<State, RuntimeError>> processInstruction(State state, Instruction instruction) {
-        final var next = state.next();
-        final var operation = instruction.operation();
-        final var addressOrValue = instruction.addressOrValue();
-        return switch (operation) {
-            case Nothing -> Optional.of(new Ok<>(next));
-            case InStore -> Optional.of(new Ok<>(next.inAndStore(addressOrValue)));
-            case Halt -> Optional.empty();
-            case Jump -> Optional.of(new Ok<>(next.jump(addressOrValue)));
-            case LoadValue -> Optional.of(new Ok<>(next.loadValue(addressOrValue)));
-            case LoadDirect -> Optional.of(next.loadDirect(addressOrValue));
-            case StoreIndirect -> Optional.of(new Ok<>(next.storeIndirect(addressOrValue)));
-            case StoreDirect -> Optional.of(new Ok<>(next.storeDirect(addressOrValue)));
-            case AddValue -> Optional.of(new Ok<>(next.addValue(addressOrValue)));
-            case AddAddress -> Optional.of(new Ok<>(next.addAddress(addressOrValue)));
-            case SubtractValue -> Optional.of(new Ok<>(next.subtractValue(addressOrValue)));
-            case LoadIndirect -> Optional.of(next.loadIndirect(addressOrValue));
-        };
-    }
-
-    private static Result<Instruction, RuntimeError> decode(Integer instruction1) {
-        return Instruction.decode(instruction1)
-                .<Result<Instruction, RuntimeError>>map(Ok::new)
-                .orElseGet(() -> new Err<>(new RuntimeError("Unknown instruction", Integer.toHexString(instruction1))));
     }
 }
