ac9b74d3afc7bbd4b577c5db82dee331ae5083cc
SirMathhman <sirmathhman@gmail.com>
2024-12-12 09:11:08 -0500

    Nuke old code

--- src/java/magma/Results.java
+++
@@ -1,9 +0,0 @@
-package magma;
-
-import java.util.Optional;
-
-public class Results {
-    public static <T, X> Optional<Result<T, X>> invertOption(Result<Optional<T>, X> result) {
-        return result.match(value -> value.map(Ok::new), err -> Optional.of(new Err<>(err)));
-    }
-}
--- src/java/magma/RuntimeError.java
+++
@@ -1,7 +0,0 @@
-package magma;
-
-public record RuntimeError(String message, String context) {
-    public String display() {
-        return message + ": " + context;
-    }
-}
