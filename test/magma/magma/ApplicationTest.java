618792a4ed814d97e24e37f3192385a1a8821524
SirMathhman <sirmathhman@gmail.com>
2024-12-12 15:53:05 -0500

    Return statements

--- test/magma/magma/ApplicationTest.java
+++ test/magma/magma/ApplicationTest.java
@@ -8,8 +8,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+
+    public static final String RETURN_KEYWORD_WITH_SPACE = "return ";
+    public static final String STATEMENT_END = ";";
+
     private static Result<String, RuntimeError> run(String input) {
         if (input.isEmpty()) return new Ok<>("");
+        if (input.startsWith(RETURN_KEYWORD_WITH_SPACE)) {
+            final var slice = input.substring(RETURN_KEYWORD_WITH_SPACE.length());
+            if(slice.endsWith(STATEMENT_END)) {
+                return new Ok<>(slice.substring(0, slice.length() - STATEMENT_END.length()));
+            }
+        }
 
         try {
             Integer.parseInt(input);
@@ -19,8 +29,16 @@ public class ApplicationTest {
         }
     }
 
-    private static void assertRun(String input) {
-        assertEquals(input, run(input).findValue().orElseThrow());
+    private static void assertRun(String output) {
+        assertRun(output, output);
+    }
+
+    private static void assertRun(String output, String input) {
+        assertEquals(output, run(input).findValue().orElseThrow());
+    }
+
+    private static String renderReturn(String value) {
+        return RETURN_KEYWORD_WITH_SPACE + value + STATEMENT_END;
     }
 
     @Test
@@ -37,5 +55,11 @@ public class ApplicationTest {
     @Test
     void invalidSymbol() {
         assertTrue(run("x").isErr());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"100", "200"})
+    void returns(String value) {
+        assertRun(value, renderReturn(value));
     }
 }
