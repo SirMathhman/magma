4ef4b5aa8df9228336d23813cccb57fe2609074c
SirMathhman <sirmathhman@gmail.com>
2024-12-12 09:14:35 -0500

    Root segments?

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,15 +3,53 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
             final var source = Paths.get(".", "working", "main.mgs");
             final var input = Files.readString(source);
-            Files.writeString(source.resolveSibling("main.casm"), input);
+            Files.writeString(source.resolveSibling("main.casm"), compile(input));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String compile(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == '}' && depth == 1) {
+                depth--;
+                advance(buffer, segments);
+                buffer = new StringBuilder();
+            } else if (c == ';' && depth == 0) {
+                advance(buffer, segments);
+                buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        advance(buffer, segments);
+
+        var output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static String compileRootSegment(String rootSegment) {
+        return rootSegment;
+    }
+
+    private static void advance(StringBuilder buffer, ArrayList<String> segments) {
+        if (!buffer.isEmpty()) segments.add(buffer.toString());
     }
 }
