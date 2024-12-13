66ef1f8ecbc3a50aa4b45eef70d20963f768ffe8
SirMathhman <sirmathhman@gmail.com>
2024-12-13 16:17:33 -0500

    Weird state issues

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -10,15 +10,20 @@ import static magma.Operation.*;
 public class Main {
     public static void main(String[] args) {
         var input = new LinkedList<>(Stream.of(
-                set(2, Jump.of(0)),
-                set(3, NoOp.empty()),
-                set(4, 5),
-                set(5, 0),
-                set(2, Jump.of(3))
+                setAbsolute(2, Jump.of(0)),
+                setAbsolute(3, NoOp.empty()),
+                setAbsolute(4, 6),
+                setAbsolute(5, 0),
+                List.of(
+                        LoadValue.of(0x100),
+                        StoreIndirectly.of(4)
+                ),
+                setAbsolute(2, Jump.of(3))
         ).flatMap(Collection::stream).toList());
 
         var memory = new ArrayList<>(Collections.singletonList(InAndStore.of(1)));
         var programCounter = 0;
+        var accumulator = 0;
 
         while (programCounter < memory.size()) {
             final var instruction = memory.get(programCounter);
@@ -32,11 +37,22 @@ public class Main {
                 case InAndStore -> {
                     final var next = input.isEmpty() ? 0 : input.pollFirst();
                     set(memory, addressOrValue, next);
+                    break;
                 }
                 case Jump -> {
                     programCounter = addressOrValue;
+                    break;
                 }
                 case NoOp -> {
+                    break;
+                }
+                case LoadValue -> {
+                    accumulator = addressOrValue;
+                    break;
+                }
+                case StoreIndirectly -> {
+                    final var address = memory.get(addressOrValue);
+                    set(memory, address, accumulator);
                 }
             }
         }
@@ -44,18 +60,20 @@ public class Main {
         System.out.println(display(memory));
     }
 
-    private static List<Integer> set(int address, int instruction) {
+    private static List<Integer> setAbsolute(int address, int instruction) {
         return List.of(InAndStore.of(address), instruction);
     }
 
     private static String display(ArrayList<Integer> memory) {
         return IntStream.range(0, memory.size())
-                .mapToObj(index -> {
-                    final var value = memory.get(index);
-                    final var instruction = Instruction.decode(value);
-                    return Integer.toHexString(index) + ") " + instruction;
-                })
+                .mapToObj(index -> displayWithIndex(memory, index))
                 .collect(Collectors.joining("\n"));
+    }
+
+    private static String displayWithIndex(ArrayList<Integer> memory, int index) {
+        final var value = memory.get(index);
+        final var instruction = Instruction.decode(value);
+        return Integer.toHexString(index) + ") " + instruction;
     }
 
     private static void set(List<Integer> memory, int address, int value) {
--- src/java/magma/Operation.java
+++ src/java/magma/Operation.java
@@ -1,7 +1,7 @@
 package magma;
 
 public enum Operation {
-    InAndStore, Jump, NoOp;
+    InAndStore, Jump, NoOp, LoadValue, StoreIndirectly;
 
     public int of(int addressOrValue) {
         return (ordinal() << 24) + addressOrValue;
