6944eb835ef6ce3d5ef3cef7d60d08f9b8ad433b
SirMathhman <sirmathhman@gmail.com>
2024-12-13 16:08:37 -0500

    Instruction OK

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,28 +1,66 @@
 package magma;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static magma.Operation.*;
 
 public class Main {
     public static void main(String[] args) {
         var input = new LinkedList<Integer>(List.of(
-
+                InAndStore.of(2),
+                Jump.of(0),
+                InAndStore.of(3),
+                NoOp.empty(),
+                InAndStore.of(2),
+                Jump.of(3)
         ));
 
-        var memory = new ArrayList<Integer>();
+        var memory = new ArrayList<>(Collections.singletonList(InAndStore.of(1)));
         var programCounter = 0;
 
         while (programCounter < memory.size()) {
             final var instruction = memory.get(programCounter);
-            final var opCode = instruction << 24;
+            final var opCode = instruction >> 24;
             final var addressOrValue = instruction & 0x00FFFFFF;
 
             programCounter++;
 
-            final var operation = Operation.values()[opCode];
+            final var operation = values()[opCode];
             switch (operation) {
+                case InAndStore -> {
+                    final var next = input.pollFirst();
+                    set(memory, addressOrValue, next);
+                }
+                case Jump -> {
+                    programCounter = addressOrValue;
+                }
+                case NoOp -> {
+                }
             }
         }
+
+        System.out.println(display(memory));
+    }
+
+    private static String display(ArrayList<Integer> memory) {
+        return IntStream.range(0, memory.size())
+                .mapToObj(index -> {
+                    final var value = memory.get(index);
+                    final var instruction = Instruction.decode(value);
+                    return Integer.toHexString(index) + ") " + instruction;
+                })
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static void set(List<Integer> memory, int address, int value) {
+        while (!(address < memory.size())) {
+            memory.add(0);
+        }
+        memory.set(address, value);
     }
 }
--- src/java/magma/Operation.java
+++ src/java/magma/Operation.java
@@ -1,4 +1,13 @@
 package magma;
 
 public enum Operation {
+    InAndStore, Jump, NoOp;
+
+    public int of(int addressOrValue) {
+        return (ordinal() << 24) + addressOrValue;
+    }
+
+    public int empty() {
+        return of(0);
+    }
 }
