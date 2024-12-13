49d2c4f718177bbad85f27f80290ee270d0b1b85
SirMathhman <sirmathhman@gmail.com>
2024-12-13 15:59:40 -0500

    Stuff

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,28 @@
 package magma;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        var input = new LinkedList<Integer>(List.of(
 
+        ));
+
+        var memory = new ArrayList<Integer>();
+        var programCounter = 0;
+
+        while (programCounter < memory.size()) {
+            final var instruction = memory.get(programCounter);
+            final var opCode = instruction << 24;
+            final var addressOrValue = instruction & 0x00FFFFFF;
+
+            programCounter++;
+
+            final var operation = Operation.values()[opCode];
+            switch (operation) {
+            }
+        }
     }
 }
