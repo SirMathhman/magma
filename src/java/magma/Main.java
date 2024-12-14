6057f8d6ad92cd405aec6939acc367bda1c132be
SirMathhman <sirmathhman@gmail.com>
2024-12-14 13:10:19 -0500

    Split root segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,8 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,11 +14,42 @@ public class Main {
             final var target = source.resolveSibling("Main.mgs");
             Files.writeString(target, compile(input));
         } catch (IOException | CompileException e) {
+            //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
     }
 
     private static String compile(String input) throws CompileException {
-        throw new CompileException("Unknown root", input);
+        final var segments = split(input);
+
+        var output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static List<String> split(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                append(buffer, segments);
+                buffer = new StringBuilder();
+            }
+        }
+        append(buffer, segments);
+        return segments;
+    }
+
+    private static String compileRootSegment(String segment) throws CompileException {
+        throw new CompileException("Unknown root segment", segment);
+    }
+
+    private static void append(StringBuilder buffer, ArrayList<String> segments) {
+        if (!buffer.isEmpty()) segments.add(buffer.toString());
     }
 }
