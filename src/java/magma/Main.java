e522c32a5f7aac1ac9230079f0d1b333dcea853a
SirMathhman <sirmathhman@gmail.com>
2024-12-15 16:35:01 -0500

    Root segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,7 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +18,36 @@ public class Main {
     }
 
     private static String compile(String input) throws CompileException {
-        throw new CompileException("Unknown root", input);
+        final var segments = split(input);
+
+        var output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compileRootSegment(segment));
+        }
+
+        return output.toString();
+    }
+
+    private static ArrayList<String> split(String input) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            var c = input.charAt(i);
+            buffer.append(c);
+            if (c == ';') {
+                advance(buffer, segments);
+                buffer = new StringBuilder();
+            }
+        }
+        advance(buffer, segments);
+        return segments;
+    }
+
+    private static void advance(StringBuilder buffer, ArrayList<String> segments) {
+        if (!buffer.isEmpty()) segments.add(buffer.toString());
+    }
+
+    private static String compileRootSegment(String input) throws CompileException {
+        throw new CompileException("Unknown root segment", input);
     }
 }
