5d389bbf0af9ca57221517c7daf7a237da2cb061
SirMathhman <sirmathhman@gmail.com>
2024-12-15 16:30:14 -0500

    Fix path

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         try {
-            final var source = Paths.get(".", "src", "magma", "Main.java");
+            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
             final var input = Files.readString(source);
             final var target = source.resolveSibling("Main.mgs");
             Files.writeString(target, compile(input));
