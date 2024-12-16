6421af3febdd4c965d35a3eb86ceb0d77e4a1913
SirMathhman <sirmathhman@gmail.com>
2024-12-15 23:05:33 -0500

    Cleanup

--- src/java/magma/option/Options.java
+++ src/java/magma/option/Options.java
@@ -4,6 +4,6 @@ import java.util.stream.Stream;
 
 public class Options {
     public static <T> Stream<T> asStream(Option<T> option) {
-        return option.<Stream<T>>map(Stream::of).orElseGet(Stream::empty);
+        return option.map(Stream::of).orElseGet(Stream::empty);
     }
 }
