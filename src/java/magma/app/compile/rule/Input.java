6d7cce17651e35632e54044c7dcb036a00ca0f14
SirMathhman <sirmathhman@gmail.com>
2024-12-18 12:11:15 -0500

    OK

--- src/java/magma/app/compile/rule/Input.java
+++ src/java/magma/app/compile/rule/Input.java
@@ -1,11 +1,26 @@
 package magma.app.compile.rule;
 
-public record Input(String input) {
+public record Input(String input, int start, int length) {
+    public Input(String input) {
+        this(input, 0, input.length());
+    }
+
     public String getInput() {
         return input;
     }
 
     public String display() {
-        return input;
+        var buffer = new StringBuilder().append("\n");
+        final var slices = computeSlice().split("\\n");
+        for (int i = 0; i < slices.length; i++) {
+            var slice = slices[i];
+            buffer.append(slice).append("\n");
+            buffer.append("^".repeat(slice.length())).append("\n");
+        }
+        return buffer.toString();
+    }
+
+    private String computeSlice() {
+        return input.substring(start, start + length);
     }
 }
