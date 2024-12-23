af88a2dccf4f15668add59708a41755d9d07d261
SirMathhman <sirmathhman@gmail.com>
2024-12-22 19:44:05 -0500

    Generate Temp

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,19 +3,57 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         try {
             final var source = Paths.get(".", "src", "java", "magma", "Main.java");
             final var input = Files.readString(source);
-            compileRoot(input);
+            final var output = compileRoot(input);
+            final var target = source.resolveSibling("Temp.java");
+            Files.writeString(target, output);
         } catch (IOException | CompileException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void compileRoot(String root) throws CompileException {
-        throw new CompileException("Unknown root", root);
+    private static String compileRoot(String root) throws CompileException {
+        final var segments = split(root);
+        final var buffer = new StringBuilder();
+        for (String segment : segments) {
+            buffer.append(compileRootSegment(segment.strip()));
+        }
+        return buffer.toString();
+    }
+
+    private static ArrayList<String> split(String root) {
+        var segments = new ArrayList<String>();
+        var buffer = new StringBuilder();
+        var depth = 0;
+        for (int i = 0; i < root.length(); i++) {
+            var c = root.charAt(i);
+            buffer.append(c);
+            if (c == ';' && depth == 0) {
+                advance(buffer, segments);
+                buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
+            }
+        }
+        advance(buffer, segments);
+        return segments;
+    }
+
+    private static void advance(StringBuilder buffer, ArrayList<String> segments) {
+        if (!buffer.isEmpty()) segments.add(buffer.toString());
+    }
+
+    private static String compileRootSegment(String rootSegment) throws CompileException {
+        if (rootSegment.startsWith("package ")) return "package temp;";
+        if (rootSegment.startsWith("import ")) return "import temp;";
+        if (rootSegment.contains("class ")) return "class Temp {}";
+        throw new CompileException("Unknown root segment", rootSegment);
     }
 }
