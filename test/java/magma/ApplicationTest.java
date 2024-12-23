ddc071f40314055ed88fbba3973fb6e7f57d8adc
SirMathhman <sirmathhman@gmail.com>
2024-12-23 09:40:44 -0500

    Generate a target

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -1,15 +1,42 @@
 package magma;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
+    public static final Path SOURCE = Paths.get(".", "temp.java");
+    public static final Path TARGET = Paths.get(".", "temp.mgs");
+
+    private static void run() throws IOException {
+        if (Files.exists(SOURCE)) {
+            Files.createFile(TARGET);
+        }
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        Files.deleteIfExists(TARGET);
+        Files.deleteIfExists(SOURCE);
+    }
+
     @Test
-    void generateNothing() {
-        assertFalse(Files.exists(Paths.get(".", "temp.mgs")));
+    void generateNothing() throws IOException {
+        run();
+        assertFalse(Files.exists(TARGET));
+    }
+
+    @Test
+    void generateSomething() throws IOException {
+        Files.createFile(SOURCE);
+        run();
+        assertTrue(Files.exists(TARGET));
     }
 }
