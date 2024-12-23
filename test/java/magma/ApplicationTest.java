24432a083f30ae04d2c4d9d715a40fd90d148a2f
SirMathhman <sirmathhman@gmail.com>
2024-12-23 09:43:11 -0500

    Invalidate test

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -8,17 +8,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationTest {
     public static final Path SOURCE = Paths.get(".", "temp.java");
     public static final Path TARGET = Paths.get(".", "temp.c");
 
-    private static void run() throws IOException {
-        if (Files.exists(SOURCE)) {
-            Files.createFile(TARGET);
-        }
+    private static void run() throws IOException, CompileException {
+        if (!Files.exists(SOURCE)) return;
+
+        final var input = Files.readString(SOURCE);
+        final var output = input.isEmpty() ? "" : compileInput(input);
+        Files.writeString(TARGET, output);
+    }
+
+    private static String compileInput(String root) throws CompileException {
+        throw new CompileException("Unknown root", root);
+    }
+
+    private static void runWithInput(String input) throws IOException, CompileException {
+        Files.writeString(SOURCE, input);
+        run();
     }
 
     @AfterEach
@@ -28,15 +38,19 @@ public class ApplicationTest {
     }
 
     @Test
-    void generateNothing() throws IOException {
+    void invalidate() {
+        assertThrows(CompileException.class, () -> runWithInput("test"));
+    }
+
+    @Test
+    void generateNothing() throws IOException, CompileException {
         run();
         assertFalse(Files.exists(TARGET));
     }
 
     @Test
-    void generateSomething() throws IOException {
-        Files.createFile(SOURCE);
-        run();
+    void generateSomething() throws IOException, CompileException {
+        runWithInput("");
         assertTrue(Files.exists(TARGET));
     }
 }
