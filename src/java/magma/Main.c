9ec688483fe38c870a8f74ffc9acedfec2c845b1
SirMathhman <sirmathhman@gmail.com>
2024-12-26 09:01:42 -0500

    Recompile

--- src/java/magma/Main.c
+++ src/java/magma/Main.c
@@ -2,26 +2,8 @@
 #include "magma/compile/Node.h"
 #include "magma/compile/error/ApplicationError.h"
 #include "magma/compile/error/JavaError.h"
-#include "magma/compile/rule/DiscardRule.h"
-#include "magma/compile/rule/ExactRule.h"
-#include "magma/compile/rule/LazyRule.h"
-#include "magma/compile/rule/split/LocatingSplitter.h"
-#include "magma/compile/rule/slice/NodeListRule.h"
-#include "magma/compile/rule/OrRule.h"
-#include "magma/compile/rule/string/PrefixRule.h"
-#include "magma/compile/rule/Rule.h"
-#include "magma/compile/rule/split/SplitRule.h"
-#include "magma/compile/rule/string/StringListRule.h"
-#include "magma/compile/rule/string/StringRule.h"
-#include "magma/compile/rule/string/StripRule.h"
-#include "magma/compile/rule/string/SuffixRule.h"
-#include "magma/compile/rule/string/SymbolRule.h"
-#include "magma/compile/rule/TypeRule.h"
-#include "magma/compile/rule/split/locate/BackwardsLocator.h"
-#include "magma/compile/rule/split/locate/FirstLocator.h"
-#include "magma/compile/rule/split/locate/LastLocator.h"
-#include "magma/compile/rule/slice/StatementSlicer.h"
-#include "magma/compile/rule/slice/TypeSlicer.h"
+#include "magma/compile/lang/CLang.h"
+#include "magma/compile/lang/JavaLang.h"
 #include "java/io/IOException.h"
 #include "java/nio/file/Files.h"
 #include "java/nio/file/Path.h"
@@ -40,22 +22,5 @@ struct Main {
 	Tuple<State, Node> passNodeLists(){}
 	Tuple<State, Node> modify(){}
 	Optional<ApplicationError> writeGenerated(){}
-	Rule createCRootRule(){}
-	OrRule createCRootMemberRule(){}
-	Rule createStructRule(){}
-	Rule createGroupRule(){}
-	Rule createStructMemberRule(){}
-	TypeRule createFunctionRule(){}
-	Rule createJavaRootRule(){}
-	TypeRule createClassRule(){}
-	SplitRule wrapInBlock(){}
-	Rule createClassMemberRule(){}
-	TypeRule createMethodRule(){}
-	Rule createTypeRule(){}
-	TypeRule createSymbolRule(){}
-	TypeRule createGenericRule(){}
-	TypeRule createWhitespaceRule(){}
-	Rule createNamespacedRule(){}
-	Rule createIncludesRule(){}
 }
 
