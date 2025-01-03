4a4cf3c6473e344a40f2496ad82f52f558bc792a
SirMathhman <sirmathhman@gmail.com>
2025-01-02 22:54:09 -0500

    Hmmmm

---
+++ src/c/magma/app/CompileException.c
@@ -0,0 +1,11 @@
+package magma.app;package magma.app;
+
+public class CompileException extends Exception {
+    public CompileException(String message, String context) {
+        super(message + ": " + context);package magma.app;
+
+public class CompileException extends Exception {
+    public CompileException(String message, String context) {
+        super(message + ": " + context);
+    }
+}
