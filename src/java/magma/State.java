24cefd37ad4b2204a3e5598a9e565bca6b56e221
SirMathhman <sirmathhman@gmail.com>
2025-01-03 22:25:34 -0500

    Split OK

--- src/java/magma/State.java
+++ src/java/magma/State.java
@@ -5,15 +5,17 @@ import java.util.List;
 
 public class State {
     public final List<String> segments;
-    public StringBuilder buffer;
+    public final StringBuilder buffer;
+    public final int depth;
 
-    public State(List<String> segments, StringBuilder buffer) {
+    public State(List<String> segments, StringBuilder buffer, int depth) {
         this.buffer = buffer;
         this.segments = segments;
+        this.depth = depth;
     }
 
     public State() {
-        this(new ArrayList<>(), new StringBuilder());
+        this(new ArrayList<>(), new StringBuilder(), 0);
     }
 
     State advance() {
@@ -21,10 +23,22 @@ public class State {
 
         final var copy = new ArrayList<>(this.segments);
         copy.add(this.buffer.toString());
-        return new State(copy, new StringBuilder());
+        return new State(copy, new StringBuilder(), depth);
     }
 
     State append(char c) {
-        return new State(segments, buffer.append(c));
+        return new State(segments, buffer.append(c), depth);
+    }
+
+    public boolean isLevel() {
+        return depth == 0;
+    }
+
+    public State enter() {
+        return new State(segments, buffer, depth + 1);
+    }
+
+    public State exit() {
+        return new State(segments, buffer, depth - 1);
     }
 }
