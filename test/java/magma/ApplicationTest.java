028bb104ec1de2d1c9c5cc9bc40e219ab9efa633
SirMathhman <sirmathhman@gmail.com>
2025-01-04 18:08:38 -0500

    Invalidate

--- test/java/magma/ApplicationTest.java
+++ test/java/magma/ApplicationTest.java
@@ -9,11 +9,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ApplicationTest {
     public static final Path SOURCE_PATH = Paths.get(".", "Main.java");
     public static final Path TARGET_PATH = Paths.get(".", "Main.c");
+
+    private static void runWithInput(String input) throws IOException, CompileException {
+        Files.writeString(SOURCE_PATH, input);
+        new Application(new SingleSourceSet(SOURCE_PATH)).run();
+    }
+
+    @Test
+    void invalidate() {
+        assertThrows(CompileException.class, () -> runWithInput("100"));
+    }
 
     @AfterEach
     void tearDown() throws IOException {
@@ -22,9 +33,8 @@ public class ApplicationTest {
     }
 
     @Test
-    void generateTarget() throws IOException {
-        Files.createFile(SOURCE_PATH);
-        new Application(new SingleSourceSet(SOURCE_PATH)).run();
+    void generateTarget() throws CompileException, IOException {
+        runWithInput("");
         assertTrue(Files.exists(TARGET_PATH));
     }
 
