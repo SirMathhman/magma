67897630cdc630c2cb7c6cf4a2df7c54af9a9153
SirMathhman <sirmathhman@gmail.com>
2025-01-04 17:55:22 -0500

    Delete

--- src/c/magma/Main.c
+++
@@ -1,7 +0,0 @@
-#include "java/io/IOException.h"
-#include "java/nio/file/Files.h"
-#include "java/nio/file/Path.h"
-#include "java/nio/file/Paths.h"
-#include "java/util/List.h"
-#include "java/util/stream/Collectors.h"
-struct Temp {};
