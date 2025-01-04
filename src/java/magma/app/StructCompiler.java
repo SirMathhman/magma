a7b031f5a93949ea8ba21dd5c6dcb14a1b9aaf78
SirMathhman <sirmathhman@gmail.com>
2025-01-03 19:46:28 -0500

    Definitions

--- src/java/magma/app/StructCompiler.java
+++ src/java/magma/app/StructCompiler.java
@@ -5,6 +5,13 @@ import magma.app.compile.CompileException;
 import java.util.Optional;
 
 public record StructCompiler(String infix) implements Compiler {
+    private static String compileStatement(String classSegment) throws CompileException {
+        if (classSegment.startsWith("if ")) return "if (true) {}";
+        if (classSegment.contains("=")) return "int value = 0;";
+
+        throw new CompileException("Unknown class segment", classSegment);
+    }
+
     @Override
     public Optional<String> compile(String input) throws CompileException {
         if (!input.contains(infix)) return Optional.empty();
@@ -27,7 +34,7 @@ public record StructCompiler(String infix) implements Compiler {
 
     private String compileClassSegment(String classSegment) throws CompileException {
         final var method = compileMethod(classSegment);
-        if(method.isPresent()) {
+        if (method.isPresent()) {
             return method.get();
         }
 
@@ -36,22 +43,18 @@ public record StructCompiler(String infix) implements Compiler {
 
     private Optional<String> compileMethod(String classSegment) throws CompileException {
         final var contentStart = classSegment.indexOf('{');
-        if(contentStart == -1) return Optional.empty();
+        if (contentStart == -1) return Optional.empty();
 
         final var contentEnd = classSegment.lastIndexOf('}');
-        if(contentEnd == -1) return Optional.empty();
+        if (contentEnd == -1) return Optional.empty();
 
         final var content = classSegment.substring(contentStart + 1, contentEnd);
         final var segments = Splitter.split(content);
         var buffer = new StringBuilder();
         for (String segment : segments) {
-            buffer.append(compileUnknownStatement(segment));
+            buffer.append(compileStatement(segment.strip()));
         }
 
         return Optional.of(buffer.toString());
-    }
-
-    private static String compileUnknownStatement(String classSegment) throws CompileException {
-        throw new CompileException("Unknown class segment", classSegment);
     }
 }
