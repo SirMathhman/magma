7c02d30843fee4b39519c3f79b00a9b9d9bfa5fe
SirMathhman <sirmathhman@gmail.com>
2025-01-06 17:36:04 -0500

    Split

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -3,6 +3,8 @@ package magma;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +21,37 @@ public class Main {
     }
 
     private static String compile(String root) throws CompileException {
-        throw new CompileException("Invalid root", root);
+        final var segments = split(root);
+
+        var output = new StringBuilder();
+        for (String segment : segments) {
+            output = output.append(compileRootMember(segment));
+        }
+
+        return output.toString();
     }
+
+    private static List<String> split(String root) {
+        var state = new State();
+        for (int i = 0; i < root.length(); i++) {
+            var c = root.charAt(i);
+            state = splitAtChar(state, c);
+        }
+
+        return state.advance().segments();
+    }
+
+    private static State splitAtChar(State state, char c) {
+        final var appended = state.append(c);
+        if (c == ';') {
+            return appended.advance();
+        } else {
+            return appended;
+        }
+    }
+
+    private static String compileRootMember(String rootSegment) throws CompileException {
+        throw new CompileException("Invalid root segment", rootSegment);
+    }
+
 }
