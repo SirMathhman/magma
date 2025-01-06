d59d5e54d4693eec227d966d9ffffaf67d59c01f
SirMathhman <sirmathhman@gmail.com>
2025-01-06 14:40:04 -0500

    Migrate out Option from Err

--- src/java/magma/result/Err.java
+++ src/java/magma/result/Err.java
@@ -3,24 +3,14 @@ package magma.result;
 import magma.option.Option;
 import magma.option.Some;
 
-import java.util.Optional;
-
 public record Err<T, X>(X error) implements Result<T, X> {
-    private Optional<T> findValue0() {
-        return Optional.empty();
-    }
-
-    private Optional<X> findError0() {
-        return Optional.of(this.error);
-    }
-
     @Override
     public Option<T> findValue() {
-        return findValue0().<Option<T>>map(Some::new).orElseGet(None::new);
+        return new None<>();
     }
 
     @Override
     public Option<X> findError() {
-        return findError0().<Option<X>>map(Some::new).orElseGet(None::new);
+        return new Some<>(this.error);
     }
 }
