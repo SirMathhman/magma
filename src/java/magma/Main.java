b7d51f38533e077fba72d5bc6826952b088e87a2
SirMathhman <sirmathhman@gmail.com>
2025-01-06 17:31:18 -0500

    Absolute bare minimum

---
+++ src/java/magma/Main.java
@@ -0,0 +1,24 @@
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            final var input = Files.readString(source);
+            final var output = compile(input);
+            final var target = source.resolveSibling("Main.c");
+            Files.writeString(target, output);
+        } catch (IOException | CompileException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+
+    private static String compile(String root) throws CompileException {
+        throw new CompileException("Invalid root", root);
+    }
+}
