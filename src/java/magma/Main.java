d70b7de806cb1a830dc46931056a1268fbfc5b64
SirMathhman <sirmathhman@gmail.com>
2025-01-07 18:28:05 -0500

    Fail

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -1,7 +1,23 @@
 package magma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            final var source = Paths.get(".", "src", "java", "magma", "Main.java");
+            final var input = Files.readString(source);
+            final var output = compile(input);
+            Files.writeString(source.resolveSibling("Main.c"), output);
+        } catch (IOException | CompileException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
 
+    private static String compile(String root) throws CompileException {
+        throw new CompileException("Invalid root", root);
     }
 }
