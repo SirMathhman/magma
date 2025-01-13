196ff7523054cde9ddb518af5aaeb5bbfbbdfe09
SirMathhman <sirmathhman@gmail.com>
2025-01-12 21:29:33 -0500

    Split at depth

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -39,12 +39,16 @@ public class Main {
     private static String compileRoot(String root) {
         var segments = new ArrayList<String>();
         var buffer = new StringBuilder();
+        var depth = 0;
         for (int i = 0; i < root.length(); i++) {
             var c = root.charAt(i);
             buffer.append(c);
-            if (c == ';') {
+            if (c == ';' && depth == 0) {
                 advance(buffer, segments);
                 buffer = new StringBuilder();
+            } else {
+                if (c == '{') depth++;
+                if (c == '}') depth--;
             }
         }
         advance(buffer, segments);
