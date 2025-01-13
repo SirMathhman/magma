5d4c4011367fe6c375bf8d88d76f120c2e114d2a
SirMathhman <sirmathhman@gmail.com>
2025-01-12 21:22:46 -0500

    Main

---
+++ src/java/magma/Main.java
@@ -0,0 +1,35 @@
+package magma;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static final Path SOURCE_DIRECTORY = Paths.get(".", "src", "java");
+    public static final Path TARGET_DIRECTORY = Paths.get(".", "src", "c");
+
+    public static void main(String[] args) {
+        try (final var stream = Files.walk(SOURCE_DIRECTORY)) {
+            final var sources = stream.filter(Files::isRegularFile)
+                    .filter(file -> file.toString().endsWith(".java"))
+                    .collect(Collectors.toSet());
+
+            for (Path source : sources) {
+                final var relativized = SOURCE_DIRECTORY.relativize(source);
+                final var parent = relativized.getParent();
+                final var name = relativized.getFileName().toString();
+                final var nameWithoutExt = name.substring(0, name.indexOf('.'));
+                final var targetParent = TARGET_DIRECTORY.resolve(parent);
+                if (!Files.exists(targetParent)) Files.createDirectories(targetParent);
+
+                final var target = targetParent.resolve(nameWithoutExt + ".c");
+                Files.writeString(target, Files.readString(source));
+            }
+        } catch (IOException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+    }
+}
