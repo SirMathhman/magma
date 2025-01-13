3895c7f72c0fae4ab65ca2b5c311cbd7e6351b0f
SirMathhman <sirmathhman@gmail.com>
2025-01-12 22:49:47 -0500

    Invocation statements

--- src/c/magma/Main.c
+++ src/c/magma/Main.c
@@ -94,7 +94,12 @@ struct Main {
     }private static String compileStatement(String statement) {
         return compileAssignment(statement)
                 .or(() -> compileReturn(statement))
+                .or(() -> compileInvocation(statement))
                 .orElseGet(() -> invalidate("statement", statement));
+    }private static Optional<String> compileInvocation(String statement) {
+        return split(statement.strip(), "(").flatMap(inner -> truncateRight(inner.right(), ")").map(inner0 -> {
+            return generateStatement("temp()");
+        }));
     }private static Optional<String> compileReturn(String statement) {
         return truncateLeft(statement, "return").flatMap(inner -> truncateRight(inner, ";").map(inner0 -> {
             return generateStatement("return temp");
