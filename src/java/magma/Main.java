07d72aba47085cc2d4db49893d72bb8626a31275
SirMathhman <sirmathhman@gmail.com>
2025-01-14 17:18:46 -0500

    Inner

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +28,21 @@ public class Main {
     }
 
     private static String compile(String input) {
+        return splitAndCompile(input, Main::compileRootSegment);
+    }
+
+    private static String splitAndCompile(String input, Function<String, String> compiler) {
+        final var segments = split(input);
+
+        final var output = new StringBuilder();
+        for (String segment : segments) {
+            output.append(compiler.apply(segment.strip()));
+        }
+
+        return output.toString();
+    }
+
+    private static ArrayList<String> split(String input) {
         var segments = new ArrayList<String>();
         var buffer = new StringBuilder();
         var depth = 0;
@@ -43,13 +60,7 @@ public class Main {
             else if (c == '}') depth--;
         }
         advance(buffer, segments);
-
-        final var output = new StringBuilder();
-        for (String segment : segments) {
-            output.append(compileRootSegment(segment.strip()));
-        }
-
-        return output.toString();
+        return segments;
     }
 
     private static void advance(StringBuilder buffer, ArrayList<String> segments) {
@@ -61,16 +72,41 @@ public class Main {
             return "#include <" + input.substring("import ".length(), input.length() - 1) + ".h>\n";
         }
 
-        if (input.startsWith("namespace ")) {
-            final var contentStart = input.indexOf('{');
-            if(contentStart != -1) {
-                final var substring = input.substring(contentStart + 1);
-                if(substring.endsWith("}")) {
-                    return substring.substring(0, substring.length() - 1);
-                }
-            }
-        }
+        return compileNamespace(input)
+                .or(() -> compileStruct(input))
+                .orElse(input);
+    }
 
-        return input;
+    private static Optional<String> compileStruct(String input) {
+        if (!input.startsWith("struct ")) return Optional.empty();
+
+        final var contentStart = input.indexOf('{');
+        if (contentStart == -1) return Optional.empty();
+
+        final var name = input.substring("struct ".length(), contentStart).strip();
+        final var withEnd = input.substring(contentStart + 1);
+        if (!withEnd.endsWith("}")) return Optional.empty();
+
+        final var content = withEnd.substring(0, withEnd.length() - 1);
+        final var outputContent = splitAndCompile(content, Main::compileStructMember);
+        return Optional.of("struct " + name + " {" + outputContent + "\n};\n");
+    }
+
+    private static Optional<String> compileNamespace(String input) {
+        if (!input.startsWith("namespace ")) return Optional.empty();
+
+        final var contentStart = input.indexOf('{');
+        if (contentStart == -1) return Optional.empty();
+
+        final var substring = input.substring(contentStart + 1);
+        if (!substring.endsWith("}")) return Optional.empty();
+
+        final var inputContent = substring.substring(0, substring.length() - 1);
+        final var outputContent = splitAndCompile(inputContent, Main::compileRootSegment);
+        return Optional.of(outputContent);
+    }
+
+    private static String compileStructMember(String member) {
+        return member;
     }
 }
--- src/java/magma/main.c
+++ src/java/magma/main.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-    struct Result {
-    };
+struct Result {
+};
 int main() {
     return 0;
 }
