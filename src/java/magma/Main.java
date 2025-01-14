3ce11b63c957abae785bc8a2c12bbf5208203044
SirMathhman <sirmathhman@gmail.com>
2025-01-14 13:30:53 -0500

    Import segments

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -23,7 +23,7 @@ public class Main {
         final var segments = split(root);
         final var output = new StringBuilder();
         for (String segment : segments) {
-            output.append(compileRootSegment(segment));
+            output.append(compileRootSegment(segment.strip()));
         }
         return output.toString();
     }
@@ -44,7 +44,8 @@ public class Main {
     }
 
     private static String compileRootSegment(String rootSegment) throws CompileException {
-        if(rootSegment.startsWith("package ")) return "";
+        if (rootSegment.startsWith("package ")) return "";
+        if (rootSegment.startsWith("import ")) return "#include \"temp.h\"";
         throw new CompileException("Unknown root segment", rootSegment);
     }
 
