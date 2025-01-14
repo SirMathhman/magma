c31f9078af63a5837570aab072997dd394b17678
SirMathhman <sirmathhman@gmail.com>
2025-01-14 13:28:16 -0500

    CompileException

--- src/java/magma/Main.java
+++ src/java/magma/Main.java
@@ -10,10 +10,15 @@ public class Main {
             final var source = Paths.get(".", "src", "java", "magma", "Main.java");
             final var input = Files.readString(source);
             final var target = source.resolveSibling("Main.c");
-            Files.writeString(target, input);
-        } catch (IOException e) {
+            final var output = compile(input);
+            Files.writeString(target, output);
+        } catch (IOException | CompileException e) {
             //noinspection CallToPrintStackTrace
             e.printStackTrace();
         }
+    }
+
+    private static String compile(String root) throws CompileException {
+        throw new CompileException("Unknown root", root);
     }
 }
