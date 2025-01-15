a20866b4fd53b2707b441398b0c32915fbf40038
SirMathhman <sirmathhman@gmail.com>
2025-01-14 20:56:35 -0500

    openFileSecurely

--- src/java/magma/Main.c
+++ src/java/magma/Main.c
@@ -2,6 +2,29 @@
 
 typedef const char* string;
 
+typedef struct Result {
+} Result;
+
+Result Ok(FILE* value) {
+    Result this;
+    return this;
+}
+
+Result Err(errno_t error) {
+    Result this;
+    return this;
+}
+
+Result openFileSecurely(string path, string mode) {
+    FILE* file;
+    errno_t error = fopen_s(&file, path, mode);
+    if (error == 0) {
+        return Ok(file);
+    } else {
+        return Err(error);
+    }
+}
+
 int main(){
     return 0;
 }
