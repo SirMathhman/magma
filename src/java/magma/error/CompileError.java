70ef15443feeca53dc1ae22f9b2ecf7638ac9688
SirMathhman <sirmathhman@gmail.com>
2025-01-15 10:37:12 -0500

    Format nesting

--- src/java/magma/error/CompileError.java
+++ src/java/magma/error/CompileError.java
@@ -2,6 +2,7 @@ package magma.error;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CompileError implements Error {
     private final String message;
@@ -20,10 +21,14 @@ public class CompileError implements Error {
 
     @Override
     public String display() {
-        final var joinedCauses = this.causes.stream()
-                .map(CompileError::display)
+        return format(0);
+    }
+
+    private String format(int depth) {
+        final var joinedCauses = IntStream.range(0, this.causes.size())
+                .mapToObj(index -> "\n" + index + ") " + this.causes.get(index).format(depth + 1))
                 .collect(Collectors.joining());
 
-        return this.message + ": " + this.context + joinedCauses;
+        return "\t".repeat(depth) + this.message + ": " + this.context + joinedCauses;
     }
 }
