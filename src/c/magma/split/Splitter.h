385d60f4e3958bf0b63ea6a81172ee6f5de7930e
SirMathhman <sirmathhman@gmail.com>
2025-01-18 18:39:13 -0500

    Nuke

--- src/c/magma/split/Splitter.h
+++
@@ -1,9 +0,0 @@
-#include "temp.h"
-struct Splitter {
-	void advance(List<String> segments, StringBuilder buffer) {
-	}
-	StringBuilder merge(StringBuilder current, String element) {
-	}
-	List<String> split(String content) {
-	}
-};
--- src/c/magma/split/ValueSplitter.h
+++
@@ -1,8 +0,0 @@
-#include "temp.h"
-#include "temp.h"
-struct ValueSplitter implements Splitter {
-	StringBuilder merge(StringBuilder current, String element) {
-	}
-	List<String> split(String content) {
-	}
-};
