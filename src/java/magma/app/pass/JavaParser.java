b65ce7fc947ab10c14e64cd41c00e92678e5a2d4
SirMathhman <sirmathhman@gmail.com>
2025-01-24 17:43:47 -0500

    OK

--- src/java/magma/app/pass/JavaParser.java
+++ src/java/magma/app/pass/JavaParser.java
@@ -7,12 +7,20 @@ import magma.app.Node;
 import magma.app.error.CompileError;
 import magma.app.error.context.NodeContext;
 
+import java.util.List;
+
 public class JavaParser implements Passer {
+
+    public static final List<String> PRIMITIVES = List.of("String", "int");
+
     @Override
     public Result<PassUnit<Node>, CompileError> beforePass(PassUnit<Node> unit) {
-        final var value = unit.value();
-        if (value.is("symbol")) {
-            return new Err<>(new CompileError("Symbol not defined", new NodeContext(value)));
+        final var node = unit.value();
+        if (node.is("symbol")) {
+            final var value = node.findString("value").orElse("");
+            if (!PRIMITIVES.contains(value)) {
+                return new Err<>(new CompileError("Symbol not defined", new NodeContext(node)));
+            }
         }
 
         return new Ok<>(unit);
